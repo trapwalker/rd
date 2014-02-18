b2942c3f6f32dbc968becec3ed39c36e200f0fcb
svp <unknown>
2014-02-18 11:20:13 +0400

    model: Base entities model

--- model/base.py
+++ model/base.py
@@ -12,18 +12,37 @@ class Event(object):
 
 
 class Unit(object):
+    u'''Abstract class for any GEO-entities'''
     
     def __init__(self, owner, position):
         self.owner = owner
         self.events = []
-        self.position = position
+        self._position = position
 
     @property
     def position(self):
-        return None
+        return self._position
 
 
-class Station(Unit):
+class Stationary(Unit):
+    u'''Abstract class of stationary entities'''
 
-    def __init__(self, owner):
-        pass
+    def __init__(self, **kw):
+        super(Stationary, self).__init__(**kw)
+
+
+class Station(Stationary):
+    u'''Class of buildings'''
+
+    def __init__(self, **kw):
+        super(Station, self).__init__(**kw)
+
+
+class Bot(Unit):
+    u'''Class of mobile units'''
+
+    def __init__(self, **kw):
+        super(Bot, self).__init__(**kw)
+
+
+# todo: Mixin for witness units
