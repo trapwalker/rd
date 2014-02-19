92bed266245f6550a642463892d328b2eb3a9a8c
svp <unknown>
2014-02-19 18:03:20 +0400

    model: add witness interface, server classes

--- model/base.py
+++ model/base.py
@@ -14,28 +14,65 @@ class Event(object):
 # todo: class Task
 # todo: class Collision
 # todo: GEO-index
+# todo: fix side effect on edge of tile
+
+class Server(object):
+
+    def __init__(self, uid=None):
+        self.uid = uid or get_uid()
+        self.units = {}
+        self.stations = {}
+        self.robots = {}
+
+
+class LocalServer(Server):
+    pass
+
+
+class RemoteServer(Server):
+
+    def __init__(self, uid, uri):
+        super(RemoteServer, self).__init__(self, uid)
+        self.uri = uri
 
 
 class WitnessMixin(object):
     u'''Mixin class for injection of witness functionality'''
 
+    def witness_init(self):
+        self.subscribers = set()
+        self.r = 100 * 5 if isinstance(self, Stationary) else 1
+
+    def subscribe(self, s):
+        if isinstance(s, (list, tuple)):
+            s = set(s)
+        elif not isinstance(s, set):
+            s = {s}
+        self.subscribers += s
+
+    def unsubscribe(self, s):
+        if isinstance(s, (list, tuple)):
+            s = set(s)
+        elif not isinstance(s, set):
+            s = {s}
+        self.subscribers -= s
+
     @property
     def range_of_view(self):
-        range = 100
-        if isinstance(self, Stationary):
-            range *= 5
-
-        return range
+        return self.r
 
 
 class Unit(object):
     u'''Abstract class for any GEO-entities'''
 
-    def __init__(self, position, owner=None):
+    def __init__(self, server, position, owner=None):
+        self.server = server
         self.uid = get_uid()
         self.owner = owner
         self.events = []
         self._position = position
+        if isinstance(self, WitnessMixin):
+            self.witness_init()
 
     @property
     def position(self):
