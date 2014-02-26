b06e4773986ae39244bca292140e5c83dc3380e7
svp <unknown>
2014-02-26 09:36:10 +0400

    model: make stationary mixin

--- model/base.py
+++ model/base.py
@@ -43,8 +43,9 @@ class Observer(object):
 
 class PointObject(object):
 
-    def __init__(self):
+    def __init__(self, position=None):
         super(PointObject, self).__init__()
+        self._init_point = position
 
     def get_position(self):
         return None
@@ -54,15 +55,29 @@ class PointObject(object):
 
 class VisibleObject(PointObject):
 
-    def __init__(self, server, position):
+    def __init__(self, server, **kw):
         self.server = server
         self.uid = get_uid()
-        self._init_point = position
-        super(VisibleObject, self).__init__()
+        super(VisibleObject, self).__init__(**kw)
 
     def register(self, server):
         #super(VisibleObject, self).register(server)
         logging.debug('Register: %s', self.__class__.__name__)
+
+
+class Stationary(PointObject):
+    u'''Mixin for stationary objects'''
+
+```    def get_position(self):
+        return self._init_point
+
+
+class Heap(VisibleObject, Stationary):
+    u'''Heap objects thrown on the map'''
+    # todo: rearrange class tree
+    def __init__(self, items, **kw):
+        super(Heap, self).__init__(**kw)
+        self.inventory = Inventory()
 
 
 class Unit(VisibleObject):
@@ -80,25 +95,7 @@ class Unit(VisibleObject):
     # todo: tasks
 
 
-class Stationary(Unit):
-    u'''Abstract class of stationary entities'''
-
-    def __init__(self, **kw):
-        super(Stationary, self).__init__(**kw)
-
-    def get_position(self):
-        return self._init_point
-
-
-class Heap(Stationary):
-    u'''Heap objects thrown on the map'''
-    # todo: rearrange class tree
-    def __init__(self, items, **kw):
-        super(Heap, self).__init__(**kw)
-        self.inventory = Inventory()
-
-
-class Station(Stationary):
+class Station(Unit, Stationary):
     u'''Class of buildings'''
 
     def __init__(self, **kw):
