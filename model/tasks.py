c41d6b57b9212e0a4cfd9bf956fca370cf2b0de4
svp <unknown>
2014-02-27 01:06:33 +0400

    model.tasks: moving forecast

--- model/tasks.py
+++ model/tasks.py
@@ -14,6 +14,16 @@ class Task(object):
         self.owner = owner
         self.start_time = start_time or get_time()
 
+    @abstractmethod
+    def get_duration(self):
+        pass
+
+    duration = property(get_duration)
+
+    @property
+    def finish_time(self):
+        return self.start_time + self.duration
+
 
 class Moving(Task):
 
@@ -27,17 +37,22 @@ class Moving(Task):
 
 class Goto(Task):
 
-    def __init__(self, strart_point, target_point, **kw):
+    def __init__(self, start_point, target_point, **kw):
+        # todo: declare arg types
+        assert start_point != target_point # todo: epsilon test to eq
         super(Goto, self).__init__(**kw)
-        self.strart_point = strart_point
+        self.start_point = start_point
         self.target_point = target_point
 
-    @property
-    def finish_time(self):
-        pass
+        self._vector = self.target_point - self.start_point
 
+    def get_duration(self):
+        assert self.owner.max_velocity != 0
+        return self.start_point.distance(self.target_point) / float(self.owner.max_velocity)
 
-
+    def get_position(self, to_time=None):
+        to_time = to_time or get_time()
+        return self._vector.normalize() * self.owner.max_velocity * (to_time - self.start_time)
 
 
 
