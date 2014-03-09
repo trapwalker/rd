c09708a4fa83eac24df584f5d41c5f6197eb3119
svp <unknown>
2014-03-09 23:44:11 +0400

    model/units: typing

--- model/units.py
+++ model/units.py
@@ -12,7 +12,9 @@ class Unit(VisibleObject):
     def __init__(self, **kw):
         super(Unit, self).__init__(**kw)
         self._task = None
+        """@type: tasks.Task"""
         self._observer = None
+        """@type: observe.Observer"""
 
     def delete(self):
         del self.observer
@@ -20,6 +22,9 @@ class Unit(VisibleObject):
         super(Unit, self).delete()
 
     def change_observer_state(self, new_state):
+        """
+        @param new_state: bool
+        """
         observer = self._observer
         if observer:
             if new_state:
@@ -28,9 +33,15 @@ class Unit(VisibleObject):
                 self.server.static_observers.remove(observer)
 
     def get_observer(self):
+        """
+        @rtype: observe.Observer | None
+        """
         return self._observer
 
     def set_observer(self, observer):
+        """
+        @param observer: observe.Observer | None
+        """
         self.change_observer_state(False)
         self._observer = observer
         self.change_observer_state(True)
@@ -39,9 +50,15 @@ class Unit(VisibleObject):
         self.set_observer(None)
 
     def get_task(self):
+        """
+        @rtype: tasks.Task | None
+        """
         return self._task
 
     def set_task(self, task):
+        """
+        @param task: tasks.Task | None
+        """
         # old_task = self._task
         self._task = task
         # todo: remove old timeline events, add new
@@ -76,25 +93,37 @@ class Bot(Unit):
         del self.task
 
     def goto(self, position):
+        """
+        @param position: vectors.Point
+        """
         self.task = tasks.Goto(position)
 
     def get_position(self):
+        """
+        @rtype: vectors.Point
+        """
         return self.motion.position if self.motion else self.position
 
     @property
     def max_velocity(self):  # m/s
+        """
+        @rtype: float
+        """
         return BALANCE.get_MaxVelocity(self)
 
     def change_observer_state(self, new_state):
+        """
+        @param new_state: bool
+        """
         if self.motion is None:
             super(Bot, self).change_observer_state(new_state)
 
     def special_contacts_search(self):
-        if not self.motion:
+        self_motion = self.motion
+        if not self_motion:
             return super(Bot, self).special_contacts_search()
 
         contacts = self.contacts
-        self_motion = self.motion
 
         contacts_with_static = self_motion.contacts_with_static
         for obj in self.server.filter_statics(None):  # todo: GEO-index clipping
@@ -105,6 +134,9 @@ class Bot(Unit):
             contacts.extend(contacts_with_dynamic(motion))
 
     def set_task(self, task):
+        """
+        @param task: tasks.Task | None
+        """
         self.change_observer_state(False)
         old_motion = self.motion
         if old_motion:
