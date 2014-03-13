404d587558b816725b27aa2d05a8abc63a0fc6c0
svp <unknown>
2014-03-13 14:41:52 +0400

    model/contacts: Typing of contacts (see/unsee)

--- model/contacts.py
+++ model/contacts.py
@@ -4,46 +4,65 @@ from collections import OrderedDict
 from operator import itemgetter
 
 
+class KindOfContact(object):
+    __slots__ = ()
+    name = None
+
+
+class KC_See(KindOfContact):
+    name = 'See'
+
+
+class KC_Unsee(KindOfContact):
+    name = 'Unsee'
+
+
 class Contact(tuple):
-    u"""
+    """
     Predicted event associated with the task robot:
         - hit an object in the observed scope of the subject;
         - object escaped from of tile range;
         - task of object is completed.
-
-    Contact(time, subj, obj)
-
-        @param time: utils.Time
-        @param subj: units.Unit
-        @param obj: base.VisibleObject
     """
     __slots__ = ()
-    _fields = ('time', 'subj', 'obj')
+    _fields = ('time', 'subj', 'obj', 'kind')
 
     time = property(itemgetter(0), doc='Time of contact')
     subj = property(itemgetter(2), doc='Subject-contacter')
     obj = property(itemgetter(3), doc='Object-contacter')
+    kind = property(itemgetter(4), doc='Kind of contact')
 
-    def __new__(cls, time, subj, obj):
-        u"""Create new instance of Contact(time, subj, obj)"""
-        obj = tuple.__new__(cls, (time, subj, obj))
+    def __new__(cls, time, subj, obj, kind):
+        """
+        Create new instance of Contact(time, subj, obj)
+        Contact(time, subj, obj, kind)
+            @param time: utils.Time
+            @param subj: units.Unit
+            @param obj: base.VisibleObject
+            @param kind: KindOfContact
+        """
+        obj = tuple.__new__(cls, (time, subj, obj, kind))
         # todo: may be need "valid" property
         return obj
 
+    def __init__(self, *av, **kw):
+        super(Contact, self).__init__(*av, **kw)
+        # todo: review, optimize
+
     def __repr__(self):
-        u"""Return a nicely formatted representation string"""
-        return 'Contact(time=%r, subj=%r, obj=%r)' % self
+        """Return a nicely formatted representation string"""
+        return 'Contact(time=%r, subj=%r, obj=%r, kind=%r)' % self
 
     def __getnewargs__(self):
-        u"""Return self as a plain tuple.  Used by copy and pickle."""
+        """Return self as a plain tuple.  Used by copy and pickle."""
         return tuple(self)
 
     def __getstate__(self):
-        u"""Exclude the OrderedDict from pickling"""
+        """Exclude the OrderedDict from pickling"""
         pass
 
     def _asdict(self):
-        u"""Return a new OrderedDict which maps field names to their values"""
+        """Return a new OrderedDict which maps field names to their values"""
         return OrderedDict(zip(self._fields, self))
 
     __dict__ = property(_asdict)
