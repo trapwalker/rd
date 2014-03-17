aa296a1a3760526d18bf7be45315b5f4aa3fbc95
svp <unknown>
2014-03-17 18:08:09 +0400

    model/events: Emitter, Subscriber (add)

--- model/events.py
+++ model/events.py
@@ -1,5 +1,29 @@
 # -*- coding: utf-8 -*-
 
+
+class Emitter(object):
+
+    def __init__(self):
+        self.subscriptions = []
+        """@type: list[Subscription]"""
+
+    def subscribe(self, subscriber):
+        self.subscriptions.append(subscriber)
+
+    def unsubscribe(self, subscriber):
+        self.subscriptions.remove(subscriber)
+
+    def emit(self, event):
+        for subscriber in self.subscriptions:
+            subscriber.onEvent(event)
+
+
+class Subscriber(object):
+
+    def onEvent(self, event):
+        pass
+
+
 class Event(object):
     def __init__(self, eType, position=None):
         self.position = position
