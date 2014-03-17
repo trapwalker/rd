646931d38ed5513306bf4a84f9d8e9099d2ef5cf
svp <unknown>
2014-03-17 23:32:04 +0400

    model/events: doc-typing (fix)

--- model/events.py
+++ model/events.py
@@ -2,29 +2,44 @@
 
 
 class Emitter(object):
-
     def __init__(self):
         self.subscriptions = []
         """@type: list[Subscription]"""
 
     def subscribe(self, subscriber):
+        """
+        @param Subscriber subscriber: New subsriber
+        """
         self.subscriptions.append(subscriber)
 
     def unsubscribe(self, subscriber):
+        """
+        @param Subscriber subscriber: Subscriber to remove
+        """
         self.subscriptions.remove(subscriber)
 
     def emit(self, event):
+        """
+        @param Event event: New emited event
+        """
         for subscriber in self.subscriptions:
             subscriber.onEvent(event)
 
 
 class Subscriber(object):
-
-    def onEvent(self, event):
+    def on_event(self, event):
+        """
+        @param Event event: Incoming event
+        """
         pass
 
 
 class Event(object):
-    def __init__(self, eType, position=None):
+    def __init__(self, e_type, position=None):
+        """
+        @param int e_type: Event type
+        @param model.vectors.Point | None position: Event location
+        """
         self.position = position
+        self.e_type = e_type
 
