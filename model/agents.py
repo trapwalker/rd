ff4adae121d08603dad28cd391dd762a2aa62dff
svp <unknown>
2014-03-19 01:26:52 +0400

    model/agents: init

---
+++ model/agents.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from base import Object
+
+
+class Agent(Object):
+
+    def __init__(self, **kw):
+        super(Agent, self).__init__(**kw)
+
+
+class User(Agent):
+    # todo: realize
+    pass
+
+
+class AI(Agent):
+    # todo: realize in future
+    pass
