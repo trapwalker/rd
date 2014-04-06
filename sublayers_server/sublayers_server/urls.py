70a441163fbebfc9ada2e6de77deb0c8fbc1c68d
svp <unknown>
2014-04-07 00:11:27 +0400

    server/django: Project name (fix)

--- sublayers_server/sublayers_server/urls.py
+++ sublayers_server/sublayers_server/urls.py
@@ -5,7 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'sublayer_server.views.home', name='home'),
+    # url(r'^$', 'sublayers_server.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
--- sublayers_server/sublayers_server/wsgi.py
+++ sublayers_server/sublayers_server/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for sublayer_server project.
+WSGI config for sublayers_server project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sublayer_server.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sublayers_server.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
