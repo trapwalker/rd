b628a55df4fa42eb83576d17a55612bf6664a573
menkent <unknown>
2014-06-26 13:06:56 +0400

    client: в примере поставлен rotateMarker и сделано изменение скорости при нажатии стрелок вниз/вверх

--- sublayers_client/view.js
+++ sublayers_client/view.js
@@ -8,13 +8,20 @@ function redrawMap() {
     var tempPoint = user.userCar.getCurrentCoord(clock.getCurrentTime());
     var tempAngleGrad = user.userCar.getCurrentDirection(clock.getCurrentTime());
     addDivToDiv("console", "rm1", "Машинка = " + tempPoint.x + " " + tempPoint.y + "   угол = "+ tempAngleGrad);
+
+    // Перенос центра карты в центр маркера пользовательской машинки
     myMap.panTo(myMap.unproject([tempPoint.x, tempPoint.y], 16), {animate: false});
 
+    // Установка угла в для поворота иконки маркера (в градусах)
+    userCarMarker.options.angle = tempAngleGrad * 180 / Math.PI;
+    // Установка новых координат маркера
     userCarMarker.setLatLng(myMap.unproject([tempPoint.x, tempPoint.y], 16));
 
-    userCarMarker.setIcon(L.divIcon({html: '<img src="img/usercar_20.png" ' +
-       'style="-webkit-transform: rotate(' + tempAngleGrad +'rad); opacity: 1;" />', className: 'markerDiv'})); // градусы в deg
-    userCarMarker.on('click', onMouseClickMarker);
+
+
+ //   userCarMarker.setIcon(L.divIcon({html: '<img src="img/usercar_20.png" ' +
+ //      'style="-webkit-transform: rotate(' + tempAngleGrad +'rad); opacity: 1;" />', className: 'markerDiv'})); // градусы в deg
+ //   userCarMarker.on('click', onMouseClickMarker);
 }
 
 function onMouseClickMap(mouseEventObject) {
@@ -22,15 +29,15 @@ function onMouseClickMap(mouseEventObject) {
 }
 
 function onMouseMoveMap(mouseEventObject) {
-    addDivToDiv("console", "mm1", "mouseEventObject.latlng = " + mouseEventObject.latlng);
-    addDivToDiv("console", "mm2", "mouseEventObject.layerPoint = " + mouseEventObject.layerPoint);
-    addDivToDiv("console", "mm3", "mouseEventObject.containerPoint = " + mouseEventObject.containerPoint);
+    //addDivToDiv("console", "mm1", "mouseEventObject.latlng = " + mouseEventObject.latlng);
+    //addDivToDiv("console", "mm2", "mouseEventObject.layerPoint = " + mouseEventObject.layerPoint);
+    //addDivToDiv("console", "mm3", "mouseEventObject.containerPoint = " + mouseEventObject.containerPoint);
     addDivToDiv("console", "mm4", "project = " + myMap.project(mouseEventObject.latlng, myMap.getZoom()));
     addDivToDiv("console", "mm5", "zoom = " + myMap.getZoom());
 }
 
 function onMouseClickMarker(mouseEventObject) {
-    alert(1);
+    addDivToDiv("console", "mt1", "testID = " + this.testID);
 }
 
 $(document).ready(function() {
@@ -40,6 +47,7 @@ $(document).ready(function() {
          maxZoom: 16,
          zoomControl: true,
          attributionControl: false,
+         keyboard: false,
          maxBounds: ([[50.21, 35.42], [51.43, 39.44]])}).setView([50.6041, 36.5954], 13);
 
     L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
@@ -52,13 +60,16 @@ $(document).ready(function() {
     myMap.on('click', onMouseClickMap);
     myMap.on('mousemove', onMouseMoveMap);
 
-    userCarMarker = L.marker([50.21, 35.42]).addTo(myMap);
-    userCarMarker.on('click', onMouseClickMarker);
-    //userCarMarker.testID = 15;
-//    var divicon = L.divIcon({html:'<img src="bolid_small.jpg" ' +
-//    'style="-webkit-transform: rotate(-1rad);" />' }, {className: 'markerDiv'});
+    userCarMarker = L.rotatedMarker([50.21, 35.42]).addTo(myMap);
+    userCarMarker.setIcon(L.icon({
+        iconUrl: 'img/usercar_20.png',
+        iconSize: [20, 20]
+    }));
 
-//    mark.setIcon(divicon);
+    userCarMarker.on('click', onMouseClickMarker);
+
+    userCarMarker.testID = 15;
+
 
     /*
     //Тест однородности координат
@@ -74,7 +85,37 @@ $(document).ready(function() {
     //for (var j = 0; j <= 330; j+= 10)
     //    for (var i = -90; i <= 90; i+= 10) {L.marker([i, j]).addTo(myMap);}
     */
+    // Add manual control of the airplane with left and right arrow keys, just because
+    document.body.addEventListener('keydown', function(e) {
+        if (e.which == 38) {
+            // поднять скорость
+            user.userCar.track = new MoveLine(
+                    clock.getCurrentTime(),       //Время начала движения
+                    100,                          //Запас топлива
+                    1,                            //Расход топлива
+                    user.userCar.getCurrentCoord(clock.getCurrentTime()), //Начальная точка
+                    mulScalVector(user.userCar.track.speedV, 1.1),        //Скорость
+                    new Point(0, 0)             //Ускорение
+            );
+            addDivToDiv("console", "st1", "текущая скорость = " + user.userCar.track.speedV.abs());
+        }
+        if (e.which == 40) {
+            // снизить скорость
+            user.userCar.track = new MoveLine(
+                clock.getCurrentTime(),       //Время начала движения
+                100,                          //Запас топлива
+                1,                            //Расход топлива
+                user.userCar.getCurrentCoord(clock.getCurrentTime()), //Начальная точка
+                mulScalVector(user.userCar.track.speedV, 0.9),        //Скорость
+                new Point(0, 0)             //Ускорение
+            );
+            addDivToDiv("console", "st1", "текущая скорость = " + user.userCar.track.speedV.abs());
+        }
+    }, true);
 });
+
+
+
 
 var myMap;
 var userCarMarker;
