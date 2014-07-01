e92e542a34f67b1c3c09189846bb913045103799
svp <unknown>
2014-07-02 01:02:44 +0400

    server/api: API methods covering, logging DONE

--- sublayers_server/model/api_tools.py
+++ sublayers_server/model/api_tools.py
@@ -21,40 +21,54 @@ class EWrongMethodError(EAPIError):
 class EWrongParamsError(EAPIError):
     """Unsupported params format"""
 
+
 def public_method(func):
     """API public method decorator"""
-    func._public_method = True
-    return func
+    def cover(*av, **kw):
+        log_call = '{method}({params})'.format(
+            method=func.__name__,
+            params=', '.join(map(repr, av) + ['{}={!r}'.format(k, v) for k, v in kw.items()]),
+        )
+        log.info('API call: %s ...', log_call)
+        try:
+            res = func(*av, **kw)
+        except TypeError as e:
+            log.error('API error: %s !-> %r', log_call, e)
+            raise EWrongParamsError(e.message)
+        log.info('API result: %s --> %r', log_call, res)
+        return res
+
+    functools.update_wrapper(cover, func)
+    cover._public_method = True
+    return cover
 
 
 class API(object):
+    """Base API provider class"""
     def __call__(self, method, params):
         method = method.strip()
         if not method or method[0] == '_' or not hasattr(self, method):
             raise EWrongMethodError('API Method {!r} is not supported.'.format(method))
 
         func = getattr(self, method)
+
+        if not callable(func):
+            raise EWrongMethodError('{!r} is not API method.'.format(method))
+
         if not hasattr(func, '_public_method'):
-            raise EWrongMethodError('API Method {!r} is not not allowed.'.format(method))
+            raise EWrongMethodError('API Method {!r} is not allowed.'.format(method))
 
-        log_call = '{method}({params})'.format(
-            method=method,
-            params=', '.join(['{}={!r}'.format(k, v) for k, v in params.items()]),
-        )
-        log.info('API call: %s ...', log_call)
-        res = func(**params)
-        log.info('API result: %s --> %r', log_call, res)
-        return res
+        return func(**params)
 
-
-class TestAPI(API):
-    @public_method
-    def f(self, x, y=3, z=4):
-        '===some about f method==='
-        return x * 100 + y * 10 + z
-
-api = TestAPI()
 
 if __name__ == '__main__':
-    pass
-    
+    class TestAPI(API):
+        g = 13
+        @public_method
+        def f(self, x, y=3, z=4):
+            '===some about f method==='
+            return x * 100 + y * 10 + z
+
+    api = TestAPI()
+    print api.f(1, y=2)
+    print api(method='f', params=dict(x=1, y=2))
