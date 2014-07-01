ec1e608267ecedf1ddb163104f498594b3371ac7
svp <unknown>
2014-07-01 04:24:19 +0400

    server/api: API methods placeholders add

---
+++ sublayers_server/model/api_tools.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import logging
+log = logging.getLogger(__name__)
+
+
+def api_method(func):
+    # todo: doctyping
+    # todo: implement
+    return func
+
+
+def api_property(prop):
+    # todo: implement
+    return prop
+
