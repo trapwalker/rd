86aa749ae11c9298d1aaaa93d1bde05ef3d87545
svp <unknown>
2014-07-02 03:48:25 +0400

    server: code clean

--- sublayers_server/server.py
+++ sublayers_server/server.py
@@ -1,22 +1,4 @@
 #!/usr/bin/env python
-#
-# Copyright 2009 Facebook
-#
-# Licensed under the Apache License, Version 2.0 (the "License"); you may
-# not use this file except in compliance with the License. You may obtain
-# a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations
-# under the License.
-"""Simplified chat demo for websockets.
-
-Authentication, error handling, etc are left as an exercise for the reader :)
-"""
 
 import logging.config
 logging.config.fileConfig("logging.conf")
@@ -28,7 +10,6 @@ import tornado.options
 import tornado.web
 import tornado.websocket
 import os.path
-import uuid
 from tornado.options import define, options
 
 from model.event_machine import LocalServer
