4f5280da19cfbe5e397187dbe99faac6bd57b6c1
svp <unknown>
2014-07-15 00:43:35 +0400

    server/model/trajectory: rajectory calculator module add

---
+++ sublayers_server/model/trajectory.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import logging
+log = logging.getLogger(__name__)
+
+from vectors import Point as P
+
+def rv(v):
+    return abs(v) + 5
+
+
+def calc(cp, direction, v, tp, rv_func=rv):
+    """Calculate and return segmets of trajectory:
+    
+    """
+    pass
+
+
+if __name__ == '__main__':
+    pass
