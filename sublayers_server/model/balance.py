2d36f0cdf17f42f5dc162c55f8a631afee67e66b
svp <unknown>
2014-08-18 15:44:36 +0400

    server/balance: Добавлена функция вычисления радиуса контакта - учет скрытности - учет зоркости - не учтены штрафы и бнусы по скорости

--- sublayers_server/model/balance.py
+++ sublayers_server/model/balance.py
@@ -19,6 +19,34 @@ class BALANCE:
         def rv_relation(v):
             return abs(v) + 5
 
+        @staticmethod
+        def get_contact_distance(subj, obj):
+            """
+            @param subj: sublayers_server.model.base.Observer
+            @param obj: sublayers_server.model.base.VisibleObject
+            """
+            # Дистанция, на котороую субъект видит нескрытного объекта
+            subj_range_view = subj.r
+
+            # скрытность объекта [0-1]:
+            #   0.0 -- совсем нет маскировки
+            #   1.0 -- затонирован полностью, не зоркий субьект не увидит и вплотную
+            obj_obscurity = obj.obscurity if hasattr(obj, 'obscurity') else 0.0
+
+            # Зоркость субъекта [0-1] - противодействие скрытности:
+            #   0.0 -- не противодействует,
+            #   0.5 -- вдвое снижает скрытность объекта
+            #   1.0 -- полностью компенсирует любую скрытность объекта
+            subj_vigilance = subj.vigilance if hasattr(subj, 'vigilance') else 0.0  # Зоркость субьекта ()
+
+            assert 0.0 <= subj_vigilance <= 1.0, 'Subject {} vigilance not in range [0..1]'.format(subj)
+            assert 0.0 <= obj_obscurity <= 1.0, 'Object {} vigilance not in range [0..1]'.format(obj)
+            # радиус, на котором субьект заметит объекта
+            r = subj_range_view * (1 - obj_obscurity * (1 - subj_vigilance))
+
+            return
+
+
     class Weapon:
         damage = 10.0
         r = 50.0
