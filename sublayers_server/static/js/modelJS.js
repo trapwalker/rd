181e6f404d20f77e152df622cafcea6783151723
abbir <unknown>
2014-08-25 15:48:21 +0300

    client: удалил model.ts

--- sublayers_server/static/js/modelJS.js
+++ sublayers_server/static/js/modelJS.js
@@ -4,93 +4,124 @@ var __extends = this.__extends || function (d, b) {
     __.prototype = b.prototype;
     d.prototype = new __();
 };
-var Point = (function () {
-    function Point(ax, ay) {
-        this.x = ax;
-        this.y = ay;
+
+
+var MapObject = (function () {
+    function MapObject(aID) {
+        this.ID = aID;
     }
-    Point.prototype.abs = function () {
-        return Math.sqrt((this.x * this.x) + (this.y * this.y));
+
+    MapObject.prototype.getCurrentCoord = function (aClockTime) {
+        return null;
     };
-    return Point;
+
+    return MapObject;
 })();
 
-//Поворот вектора на угол aAngle(в радианах) по часовой стрелке
-function rotateVector(aPoint, aAngle) {
-    return new Point((aPoint.x * Math.cos(aAngle) - aPoint.y * Math.sin(aAngle)), (aPoint.x * Math.sin(aAngle) + aPoint.y * Math.cos(aAngle)));
-}
 
-function normVector(aPoint) {
-    return mulScalVector(aPoint, 1 / aPoint.abs());
-}
+var StaticObject = (function (_super) {
+    __extends(StaticObject, _super);
 
-function summVector(aPoint1, aPoint2) {
-    return new Point((aPoint1.x + aPoint2.x), (aPoint1.y + aPoint2.y));
-}
+    function StaticObject(aID, aCoord) {
+        _super.call(this, aID);
+        this.coord = aCoord;
+    }
 
-function subVector(aPoint1, aPoint2) {
-    return new Point((aPoint1.x - aPoint2.x), aPoint1.y - aPoint2.y);
-}
+    StaticObject.prototype.getCurrentCoord = function (aClockTime) {
+        return this.coord;
+    };
 
-function mulScalVector(aPoint, aMul) {
-    return new Point((aPoint.x * aMul), (aPoint.y * aMul));
-}
+    return StaticObject;
+})(MapObject);
 
-function mulScalVectors(aPoint1, aPoint2) {
-    return (aPoint1.x * aPoint2.x) + (aPoint1.y * aPoint2.y);
-}
 
-function getPerpendicular(aPoint) {
-    return new Point(aPoint.y, -aPoint.x);
-}
+var MapTown = (function (_super) {
+    __extends(MapTown, _super);
 
-function absVector(aPoint) {
-    return aPoint.abs();
-}
+    function MapTown(aID, aCoord, aSize) {
+        _super.call(this, aID, aCoord);
+        this.size = aSize;
+    }
 
-function angleVector(aPoint1, aPoint2) {
-    return Math.acos(mulScalVectors(aPoint1, aPoint2) / (aPoint1.abs() * aPoint2.abs())) * 180 / Math.PI;
-}
+    return MapTown;
+})(StaticObject);
 
-function angleVectorRad(aPoint1, aPoint2) {
-    return Math.acos(mulScalVectors(aPoint1, aPoint2) / (aPoint1.abs() * aPoint2.abs()));
-}
 
-// Возвращает угол против часовой стрелки от положительного направления оси X
-function angleVectorRadCCW(aPoint) {
-    var angle = angleVectorRad(aPoint, new Point(1, 0));
-    if (aPoint.y < 0)
-        angle = 2 * Math.PI - angle;
-    return normalizeAngleRad(angle);
-}
+var MapGasStation = (function (_super) {
+    __extends(MapGasStation, _super);
+    function MapGasStation(aID, aCoord) {
+        _super.call(this, aID, aCoord);
+    }
+    return MapGasStation;
+})(StaticObject);
 
-function normalizeAngleRad(angle) {
-    var pi2 = Math.PI * 2;
-    var znak = (angle > 0) ? -1 : 1;
-    for (; (angle > pi2) || (angle < 0);)
-        angle += znak * pi2;
-    return angle;
-}
 
-function getDiffAngle(angle1, angle2) {
-    var res = normalizeAngleRad(angle1) - normalizeAngleRad(angle2);
-    if (Math.abs(res) <= Math.PI)
-        return res;
-    else
-        return (2 * Math.PI - Math.abs(res)) * (res > 0 ? -1 : 1);
-}
+var DynamicObject = (function (_super) {
+    __extends(DynamicObject, _super);
 
-function radToGrad(rad) {
-    return rad * 180 / Math.PI;
-}
+    function DynamicObject(aID, aTrack) {
+        _super.call(this, aID);
+        this.track = aTrack;
+    }
 
-function gradToRad(grad) {
-    return grad * Math.PI / 180.;
-}
+    DynamicObject.prototype.getCurrentDirection = function (aClockTime) {
+        return this.track.getCurrentDirection(aClockTime);
+    };
 
-function distancePoints(aPoint1, aPoint2) {
-    return subVector(aPoint1, aPoint2).abs();
-}
+    DynamicObject.prototype.getCurrentCoord = function (aClockTime) {
+        return this.track.getCurrentCoord(aClockTime);
+    };
+
+    DynamicObject.prototype.getCurrentFuel = function (aClockTime) {
+        return this.track.getCurrentFuel(aClockTime);
+    };
+
+    DynamicObject.prototype.getCurrentSpeedAbs = function (aClockTime) {
+        return this.track.getCurrentSpeedAbs(aClockTime);
+    };
+
+    return DynamicObject;
+})(MapObject);
+
+
+var MapCar = (function (_super) {
+    __extends(MapCar, _super);
+
+    function MapCar(aID, aType, aHP, aTrack) {
+        _super.call(this, aID, aTrack);
+        this.type = aType;
+        this.hp = aHP;
+        this.fireSectors = new Array();
+    }
+
+    MapCar.prototype.AddFireSector = function (aFireSector) {
+        this.fireSectors.push(aFireSector);
+    };
+
+    MapCar.prototype.AddFireSectors = function (aSectors) {
+        for (var i = 0; i < aSectors.length; i++)
+            this.AddFireSector(aSectors[i]);
+    };
+
+    MapCar.prototype.unbindOwner = function () {
+        if (this.owner)
+            this.owner.unbindCar(this);
+        return this;
+    };
+
+    return MapCar;
+})(DynamicObject);
+
+
+var UserCar = (function (_super) {
+    __extends(UserCar, _super);
+    function UserCar(aID, aType, aHP, aMaxSpeed, aTrack) {
+        _super.call(this, aID, aType, aHP, aTrack);
+        this.maxSpeed = aMaxSpeed;
+    }
+    return UserCar;
+})(MapCar);
+
 
 var MoveTrack = (function () {
     function MoveTrack(aTimeStart, aFuelStart, aFuelDec, aDirection) {
@@ -99,6 +130,7 @@ var MoveTrack = (function () {
         this.fuelDec = aFuelDec;
         this.direction = aDirection;
     }
+
     MoveTrack.prototype.getCurrentFuel = function (aCurrentTime) {
         return this.fuelStart - this.fuelDec * ((aCurrentTime - this.timeStart));
     };
@@ -185,103 +217,6 @@ var MoveCircle = (function (_super) {
     };
     return MoveCircle;
 })(MoveTrack);
-
-var MapObject = (function () {
-    function MapObject(aID) {
-        this.ID = aID;
-    }
-    MapObject.prototype.getCurrentCoord = function (aClockTime) {
-        return null;
-    };
-    return MapObject;
-})();
-
-var StaticObject = (function (_super) {
-    __extends(StaticObject, _super);
-    function StaticObject(aID, aCoord) {
-        _super.call(this, aID);
-        this.coord = aCoord;
-    }
-    StaticObject.prototype.getCurrentCoord = function (aClockTime) {
-        return this.coord;
-    };
-    return StaticObject;
-})(MapObject);
-
-var MapTown = (function (_super) {
-    __extends(MapTown, _super);
-    function MapTown(aID, aCoord, aSize) {
-        _super.call(this, aID, aCoord);
-        this.size = aSize;
-    }
-    return MapTown;
-})(StaticObject);
-
-var MapGasStation = (function (_super) {
-    __extends(MapGasStation, _super);
-    function MapGasStation(aID, aCoord) {
-        _super.call(this, aID, aCoord);
-    }
-    return MapGasStation;
-})(StaticObject);
-
-var DynamicObject = (function (_super) {
-    __extends(DynamicObject, _super);
-    function DynamicObject(aID, aTrack) {
-        _super.call(this, aID);
-        this.track = aTrack;
-    }
-    DynamicObject.prototype.getCurrentDirection = function (aClockTime) {
-        return this.track.getCurrentDirection(aClockTime);
-    };
-
-    DynamicObject.prototype.getCurrentCoord = function (aClockTime) {
-        return this.track.getCurrentCoord(aClockTime);
-    };
-
-    DynamicObject.prototype.getCurrentFuel = function (aClockTime) {
-        return this.track.getCurrentFuel(aClockTime);
-    };
-
-    DynamicObject.prototype.getCurrentSpeedAbs = function (aClockTime) {
-        return this.track.getCurrentSpeedAbs(aClockTime);
-    };
-    return DynamicObject;
-})(MapObject);
-
-var MapCar = (function (_super) {
-    __extends(MapCar, _super);
-    function MapCar(aID, aType, aHP, aTrack) {
-        _super.call(this, aID, aTrack);
-        this.type = aType;
-        this.hp = aHP;
-        this.fireSectors = new Array();
-    }
-    MapCar.prototype.AddFireSector = function (aFireSector) {
-        this.fireSectors.push(aFireSector);
-    };
-
-    MapCar.prototype.AddFireSectors = function (aSectors) {
-        for (var i = 0; i < aSectors.length; i++)
-            this.AddFireSector(aSectors[i]);
-    };
-
-    MapCar.prototype.unbindOwner = function () {
-        if (this.owner)
-            this.owner.unbindCar(this);
-        return this;
-    };
-    return MapCar;
-})(DynamicObject);
-
-var UserCar = (function (_super) {
-    __extends(UserCar, _super);
-    function UserCar(aID, aType, aHP, aMaxSpeed, aTrack) {
-        _super.call(this, aID, aType, aHP, aTrack);
-        this.maxSpeed = aMaxSpeed;
-    }
-    return UserCar;
-})(MapCar);
 
 var User = (function () {
     function User(aID, aCredit) {
