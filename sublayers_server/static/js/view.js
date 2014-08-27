c5e10e97bbd6d29124b1f4872c33011f47548f0b
menkent <unknown>
2014-08-27 22:46:17 +0400

    client: добавление двух городов на карту.

--- sublayers_server/static/js/view.js
+++ sublayers_server/static/js/view.js
@@ -220,6 +220,15 @@ $(document).ready(function () {
     chat.setMessagesHistory(cookieStorage.historyArray);
 
 
+    // Добавление городов
+    getTownMarker(new MapTown(1, new Point(7738, 21894), 'Город #1', 20), myMap);
+    var town2 = getTownMarker(new MapTown(2, new Point(26235, 15049), 'Город #2', 20), myMap);
+
+    if (user.party.name === 'Corp')
+        town2.bindPopup('Ваша задача доставить груз к этому городу.');
+    else
+        town2.bindPopup('Ваша задача не допустить доставку груза к этому городу.');
+
 
 
     carMarkerList = new CarMarkerList({_map: myMap});
