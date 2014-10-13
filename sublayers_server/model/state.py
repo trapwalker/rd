2d24e30bbd4e123d5604426b7f8d9826facd68b6
svp <unknown>
2014-10-14 02:40:21 +0400

    model/state: Существенная переработка механизма состояний: - выделен отдельный класс-предок BaseState, который нужно зеркалировать клиентом - BaseState хранит необходимую и достаточную информацию о движении для клиента - клиент получает пуш-уведомления о состояниях и десериализует их - новый класс State(BaseState) позволяет модифицировать базовое состояние

--- sublayers_server/model/state.py
+++ sublayers_server/model/state.py
@@ -28,68 +28,44 @@ def assert_time_in_state(f):
     return update_wrapper(cover, f)
 
 
-class State(object):
+class BaseState(object):
 
-    def __init__(
-        self, t, p,
-        fi=0.0,
-        v=0.0,
-        r_min=5.0,    # m
-        v_max=28.0,   # m/s ~ 100km/h
-        ac_max=10.0,  # m/s^2 ~ 1g
-        a_accelerate=5.0,
-        a_braking=-10.0,
-        cc=0.0,
-        turn=0,
-        target_point=None,
-    ):
+    def __init__(self, t, p, fi=0.0, v=0.0, a=0.0, c=None):
         """
         @param float t: time (sec)
         @param Point p: position (m)
         @param float fi: direction (rad)
         @param float v: velocity (m/s)
-        @param float r_min: minimal turning radius (m)
-        @param float v_max: maximal possible velocity (m/s)
-        @param float ac_max: maximal centripetal acceleration (m/s**2)
-        @param float a_accelerate: typical acceleration (m/s**2)
-        @param float a_braking: typical braking (m/s**2)
-        @param float cc: Cruise speed ratio
-        @param int turn: segment trajectory turning factor: 0 - forward; 1 - CCW; -1 - CCW
-        @param Point target_point: target position
+        @param float a: linear accelerate (m/s^2)
+        @param Point|None c: current center of turning if turning is (m)
         """
-        self.target_point = None
-        self.a_accelerate = a_accelerate
-        self.a_braking = a_braking
-        self.v_max = v_max
-        self.r_min = r_min
-        self.ac_max = ac_max
-        assert ac_max > 0
-
-        self.cc = 0.0
-        self.turn = 0
         self.t0 = t
         self.p0 = p
         self.fi0 = fi
-        self.v0 = v
         assert v >= 0.0
+        self.v0 = v
+        self.a = a
+        self.c = c
 
-        self.v_cc = 0.0
-        self.t_max = None
-        self.p = self.p_linear
-        self.r = None
-        self.c = None
-        self.w0 = 0.0
-        self.e = 0.0
-        self.a = 0.0
-        self.update(cc=cc, turn=turn, target_point=target_point)
+        # Кэш вычислимых параметров
+        if c is None:
+            self._r = None
+            self._turn_sign = 0
+        else:
+            assert c != p
+            pc = p - c
+            self._r = abs(pc)
+            _turn_sign = Point.polar(1, fi).cross_mul(pc)
+            assert _turn_sign
+            self._turn_sign = 1 if _turn_sign > 0 else -1
 
-    @assert_time_in_state
-    def update(self, t=None, dt=0.0, cc=None, turn=None, target_point=None):
+    def fix(self, t=None, dt=0.0):
         """
-        @param float t: time (sec)
+        @param float t: time (sec), t == t0 by default
         @param float dt: delta time (sec)
-        @param float cc: Cruise speed ratio
-        @param int turn: segment trajectory turning factor: 0 - forward; 1 - CCW; -1 - CW
+
+        Функция приведения Состояния ко времени t+dt.
+        Базовые параметры обновляются на заданный момент.
         """
         t = (self.t0 if t is None else t) + dt
 
@@ -98,55 +74,152 @@ class State(object):
             """@type: Point"""
             self.v0 = self.v(t)
             self.fi0 = self.fi(t)
-            self.w0 = self.w(t)
             self.t0 = t
 
+    def v(self, t):
+        return self.v0 + self.a * (t - self.t0)
+
+    def fi(self, t):
+        if self.c is None:
+            return self.fi0
+
+        dt = t - self.t0
+        return self.fi0 + (0.5 * self.a * dt ** 2 + self.v0 * dt) / self._r
+
+    def p(self, t):
+        """
+        @param float t: time
+        @rtype: Point
+        """
+        if self.c is None:
+            dt = t - self.t0
+            return self.p0 + Point.polar(0.5 * self.a * dt ** 2 + self.v0 * dt, self.fi0)
+        else:
+            return self.c + Point.polar(self._r, self.fi(t) - self._turn_sign * pi * 0.5)
+
+    def as_dict(self):
+        return dict(
+            self.__dict__,
+        )
+
+    def export(self):
+        u"""
+        Представление параметров состояния для клиента.
+        """
+        return dict(
+            cls='MobileState',
+            t0=self.t0,
+            p0=self.p0,
+            fi0=self.fi0,
+            v0=self.v0,
+            a=self.a,
+            c=self.c,
+            #_r=self._r,
+            #_turn=self._turn_sign,
+        )
+
+
+class State(BaseState):
+
+    def __init__(
+        self,
+        t, p, fi=0.0, v=0.0, a=0.0, c=None,
+        cc=0.0, turn=0, target_point=None,
+
+        r_min=5.0,    # m
+        v_max=28.0,   # m/s ~ 100km/h
+        ac_max=10.0,  # m/s^2 ~ 1g
+        a_accelerate=5.0,
+        a_braking=-10.0,
+    ):
+        """
+        @param float t: time (sec)
+        @param Point p: position (m)
+        @param float fi: direction (rad)
+        @param float v: velocity (m/s)
+        @param float a: linear accelerate (m/s^2)
+        @param Point|None c: current center of turning if turning is (m)
+
+        @param float cc: Cruise speed ratio
+        @param int turn: segment trajectory turning factor: 0 - forward; 1 - CCW; -1 - CW
+        @param Point|None target_point: target point of motion
+
+        @param float r_min: minimal turning radius (m)
+        @param float v_max: maximal possible velocity (m/s)
+        @param float ac_max: maximal centripetal acceleration (m/s**2)
+        @param float a_accelerate: typical acceleration (m/s**2)
+        @param float a_braking: typical braking (m/s**2)
+        """
+        super(State, self).__init__(t, p, fi, v, a, c)
+        self.a_accelerate = a_accelerate
+        self.a_braking = a_braking
+        self.v_max = v_max
+        self.r_min = r_min
+        assert ac_max > 0
+        self.ac_max = ac_max
+
+        # just declare
+        self.t_max = None
+        self.cc = None
+        self._v_cc = None
+        self.target_point = None
+
+    def _update_by_target(self, target_point):
+        """
+        Select instruction and update State for first segment of trajectory to target_point
+        """
+        self.target_point = target_point
+        # todo: calc and store future trip distance
+
+        braking_dist = -0.5 * self.v0 ** 2 / self.a_braking
+        target_distance = self.p0.distance(target_point)
+        target_direction = self.p0.direction(target_point)
+
+        # todo: normalize angles before compare (!)
+        if abs(target_distance - braking_dist) <= EPS and (self.v0 <= EPS or target_direction == self.fi0):
+            # we have arrived or we must brake to a stop
+            self.cc = 0.0
+            target_point = None
+        elif target_distance > braking_dist and target_direction == self.fi0:
+            if self.cc < EPS:
+                self.cc = 1.0
+            # todo: accelerate
+
+    @assert_time_in_state
+    def update(self, t=None, dt=0.0, cc=None, turn=None, target_point=None):
+        """
+        @param float t: time (sec)
+        @param float dt: delta time (sec)
+        @param float cc: Cruise speed ratio
+        @param int turn: segment trajectory turning factor: 0 - forward; 1 - CCW; -1 - CW
+        @param Point|None target_point: target point of motion
+        """
+        self.fix(t=t, dt=dt)
         self.t_max = None
 
         if cc is not None:
             self.cc = cc
 
         if target_point is not None:
-            # calculation a first segment of trajectory to target_point
-            assert turn, 'Turn factor and target_point declared both in state update.'
-            braking_dist = -0.5 * self.v0 ** 2 / self.a_braking
-            target_distance = self.p0.distance(target_point)
-            target_direction = self.p0.direction(target_point)
+            assert turn is None, 'Turn factor and target_point declared both in state update.'
+            self._update_by_target(target_point)
 
-            # todo: normalize angles before compare (!)
-            if abs(target_distance - braking_dist) <= EPS and (self.v0 <= EPS or target_direction == self.fi0):
-                # we have arrived or we must brake to a stop
-                self.cc = 0.0
-                target_point = None
-            elif target_distance > braking_dist and target_direction == self.fi0:
-                if self.cc < EPS:
-                    self.cc = 1.0
-                # todo: accelerate
-
-
-        self.v_cc = self.v_max * self.cc
-        target_v = self.v_cc
-
-        # todo: self.target_point = target_point
+        self._v_cc = self.v_max * self.cc
+        target_v = self._v_cc
 
         if turn is not None:
-            self.turn = turn
-
-        if self.turn:
-            self.p = self.p_circular
-            self.r = self.v0 ** 2 / self.ac_max
-            self.c = (self.p0 + Point.polar(self.r, self.fi0 + self.turn * pi / 2.0))
-            self.w0 = self.v0 / self.r
-            if self.r < self.r_min:
-                self.r = self.r_min
-                target_v = min(target_v, sqrt(self.r * self.ac_max))
+            self._turn_sign = turn
+            if turn == 0:
+                self._r = None
+                self.c = None
             else:
-                target_v = min(target_v, self.v0)
-        else:
-            self.p = self.p_linear
-            self.r = None
-            self.c = None
-            self.w0 = 0
+                self._r = self.v0 ** 2 / self.ac_max
+                if self._r < self.r_min:
+                    self._r = self.r_min
+                    target_v = min(target_v, sqrt(self._r * self.ac_max))
+                else:
+                    target_v = min(target_v, self.v0)
+                self.c = (self.p0 + Point.polar(self._r, self.fi0 - self._turn_sign * pi / 2.0))
 
         dv = target_v - self.v0
         if dv > EPS:
@@ -159,14 +232,6 @@ class State(object):
         if self.a:
             self.t_max = self.t0 + dv / self.a
 
-        if self.turn:
-            self.e = self.a / self.r
-
-    def as_dict(self):
-        return dict(
-            self.__dict__,
-        )
-
     def __str__(self):
         return (
             '<t=[{t0:.2f}-{t_max_str}];'
@@ -174,51 +239,14 @@ class State(object):
             ' fi={fi_deg:.0f};'
             ' v={v0:.0f};'
             ' a={a:.0f};'
-            ' w={w_deg:.0f};'
-            ' e={e_deg:.0f};'
             ' turn={turn};'
-            ' cc={cc_percent:.0f}% ({v_cc:.0f}m/s)>'
+            ' cc={cc_percent:.0f}% ({_v_cc:.0f}m/s)>'
         ).format(
             fi_deg=degrees(self.fi0),
-            w_deg=degrees(self.w0),
-            e_deg=degrees(self.e),
             cc_percent=self.cc * 100,
             t_max_str='{:.2f}'.format(self.t_max) if self.t_max is not None else '',
             **self.__dict__
         )
-
-    @assert_time_in_state
-    def v(self, t):
-        return self.v0 + self.a * (t - self.t0)
-
-    @assert_time_in_state
-    def w(self, t):
-        return (self.v(t) / self.r) if self.r else 0.0
-
-    @assert_time_in_state
-    def fi(self, t):
-        if self.turn == 0:
-            return self.fi0
-
-        dt = t - self.t0
-        return self.fi0 + (0.5 * self.a * dt ** 2 + self.v0 * dt) / self.r
-
-    @assert_time_in_state
-    def p_linear(self, t):
-        """
-        @param float t: time
-        @rtype: Point
-        """
-        dt = t - self.t0
-        return self.p0 + Point.polar(0.5 * self.a * dt ** 2 + self.v0 * dt, self.fi0)
-
-    @assert_time_in_state
-    def p_circular(self, t):
-        """
-        @param float t: time
-        @rtype: Point
-        """
-        return self.c + Point.polar(self.r, self.fi(t) + self.turn * pi * 0.5)
 
 
 if __name__ == '__main__':
@@ -242,10 +270,6 @@ if __name__ == '__main__':
     def u(*av, **kw):
         if 'fi' in kw:
             kw['fi'] = radians(kw['fi'])
-        if 'w' in kw:
-            kw['w'] = radians(kw['w'])
-        if 'e' in kw:
-            kw['e'] = radians(kw['e'])
         s.update(*av, **kw)
         print s
     
