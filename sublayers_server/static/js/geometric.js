a6bddd892b27293abb8217cc703b33164bbb831f
menkent <unknown>
2014-12-07 01:42:44 +0300

    client: refactoring

--- sublayers_server/static/js/geometric.js
+++ sublayers_server/static/js/geometric.js
@@ -133,11 +133,3 @@ function stopEvent(event) {
     else // Вариант Internet Explorer:
         event.cancelBubble = true
 }
-
-
-var __extends = this.__extends || function (d, b) {
-    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
-    function __() { this.constructor = d; }
-    __.prototype = b.prototype;
-    d.prototype = new __();
-};
