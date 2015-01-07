7e5b0dec99670107de000e607cc529bdd1c0920e
svp <unknown>
2015-01-07 13:07:27 +0300

    model/tasks: indev

--- sublayers_server/model/tasks.py
+++ sublayers_server/model/tasks.py
@@ -5,12 +5,10 @@ log = logging.getLogger(__name__)
 
 from abc import ABCMeta
 
-from utils import time_log_format
-
 
 class Task(object):
     __metaclass__ = ABCMeta
-    __str_template__ = '<{self.__class__.__name__} in {self.start_time_str} [{self.status_str}]'
+    __str_template__ = '<{self.__class__.__name__} [{self.status_str}]>'
 
     def __init__(self, owner):
         """
@@ -19,8 +17,8 @@ class Task(object):
         super(Task, self).__init__()
         self.owner = owner
         self._get_time = owner.server.get_time
-        self.start_time = None
         self.is_started = False
+        self.is_paused = False
         self.is_cancelled = False
         self.is_done = False
 
@@ -28,13 +26,14 @@ class Task(object):
     def status_str(self):
         return ''.join([
             'S' if self.is_started else 's',
+            'P' if self.is_paused else 'p',
             'C' if self.is_cancelled else 'c',
             'D' if self.is_done else 'd',
         ])
 
     @property
     def is_worked(self):
-        return self.is_started and not self.is_cancelled and not self.is_done
+        return self.is_started and not self.is_cancelled and not self.is_done and not self.is_paused
 
     @property
     def classname(self):
@@ -44,49 +43,49 @@ class Task(object):
         # todo: suspend store serialization
         return dict(
             cls=self.classname,
-            start_time=self.start_time,
         )
 
-    def start(self, **kw):
+    def start(self):
         #log.debug('TASK start: %s', self)
-        self.start_time = self._get_time()
-        self.on_before_start(**kw)
-        self.is_started = True
-        self.on_after_start(**kw)
+        if not self.is_started:
+            self.on_start()
+            self.is_started = True
+        elif self.is_paused:
+            self.on_resume()
+            self.is_paused = False
 
-    def done(self, **kw):
+    def pause(self):
+        if self.is_worked:
+            self.on_pause()
+            self.is_paused = True
+        else:
+            raise TaskError  # todo: realize
+
+    def done(self):
         #log.debug('TASK done: %s', self)
-        self.on_before_end(**kw)
+        self.on_done()
         self.is_done = True
-        self.on_after_end(**kw)
 
-    def cancel(self, **kw):
+    def cancel(self):
         #log.debug('TASK cancel: %s', self)
         #raise Exception('#############')
-        self.on_before_end(**kw)
+        self.on_cancel()
         self.is_cancelled = True
-        self.on_after_end(**kw)
 
-    def on_before_start(self, **kw):
+    def on_perform(self, event):
         pass
 
-    def on_after_start(self, **kw):
+    def on_start(self):
         pass
 
-    def on_before_end(self, **kw):
+    def on_before_end(self):
         pass
 
-    def on_after_end(self, **kw):
+    def on_after_end(self):
         pass
 
     def __str__(self):
         return self.__str_template__.format(self=self)
-
-    @property
-    def start_time_str(self):
-        if self.start_time is None:
-            return "#N/A"
-        return time_log_format(self.start_time)
 
     id = property(id)
 
