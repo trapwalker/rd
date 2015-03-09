2a780983e26bf80c26cdd8c309f35e0a70f49262
svp <unknown>
2015-03-09 22:39:13 +0300

    deploy: test server init and update scripts

---
+++ upd_test.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+cd /home/sl/sublayers_test
+
+REV1=`hg identify | awk '{print $1}'`
+
+hg pull
+hg update default
+
+REV2=`hg identify | awk '{print $1}'`
+
+[ "$REV1" = "$REV2" ] && echo 'no changes' || service sl_test restart
