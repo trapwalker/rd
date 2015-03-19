2f0f5dd50be79db8b4c75b246b8c9509d1fc6ad1
svp <unknown>
2015-03-20 01:53:59 +0300

    tools: HG revision getter class ADD

--- sublayers_server/service_tools.py
+++ sublayers_server/service_tools.py
@@ -5,6 +5,7 @@ log = logging.getLogger(__name__)
 
 import signal
 import os
+import re
 
 
 def set_terminate_handler(callback):
@@ -28,3 +29,21 @@ def pidfile_save(filename):
             log.error("[FAIL] Can't store PID into the file '%s': %s", filename, e)
         else:
             log.info("[DONE] PID stored into the file '%s'", filename)
+
+
+class HGRevision(object):
+    # todo: exceptions
+    rev_parser = re.compile("([0-9a-f]+)(\+?)\s(\d+)(\+?)\s(.*)")
+    # 7f9c68086b3e+ 1815+ website
+    def __init__(self):
+        with os.popen('hg id -ibn') as f:
+            raw = f.read().rstrip('\n')
+
+        h, hp, n, np, b = self.rev_parser.match(raw).groups()
+        self.hash = h
+        self.num = int(n)
+        self.branch = b
+        self.is_changed = bool(hp or np)
+
+    def __str__(self):
+        return '{self.branch}:{self.num} [{self.hash}{cf}]'.format(self=self, cf='+' if self.is_changed else '')
