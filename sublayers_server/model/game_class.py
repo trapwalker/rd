592c408f7bdfdec7c8fce35ccfbc6e00364a95ce
svp <unknown>
2015-04-08 15:13:56 +0300

    things: sketch

---
+++ sublayers_server/model/game_class.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+import logging
+log = logging.getLogger(__name__)
+
+
+class BaseMeta(type):
+    pass
+
+
+
+def load():
+    pass
---
+++ sublayers_server/model/things.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import logging
+log = logging.getLogger(__name__)
+
+
+class ThingClass(object):
+    pass
+
+
+class Container(object):
+    def save(self, stream):
+        pass
+
+
+root = Container()
+
+
