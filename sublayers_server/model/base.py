b9d7c55afc08abaa615d9760e8d51e036885a66a
abbir <unknown>
2015-04-21 16:51:59 +0300

    server: отключили зоны (ивенты зон отсутствуют) и отключили видимость         (ивенты видимости оставили).

--- sublayers_server/model/base.py
+++ sublayers_server/model/base.py
@@ -198,10 +198,13 @@ class Observer(VisibleObject):
     def can_see(self, obj, time):
         assert not self.limbo
         assert not obj.limbo
+        '''
         dist = abs(self.position(time=time) - obj.position(time=time))
         self_p_observing_range = self.params.get('p_observing_range')
         obj_p_visibility = obj.params.get('p_visibility')
         return dist <= (self_p_observing_range.value * obj_p_visibility.value)
+        '''
+        return False
 
     # todo: check calls
 
