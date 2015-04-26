4f0dbada8be17f60f4a7ca6d71a359997b32f243
svp <unknown>
2015-04-26 04:21:08 +0300

    things: Поддержка перекрытия хначения атрибута INDEV

--- sublayers_server/model/things.py
+++ sublayers_server/model/things.py
@@ -7,33 +7,58 @@ from pprint import pprint as pp
 
 
 class Attribute(object):
-    def __init__(self):
+    def __init__(self, value=None):
         self.name = None
+        self.store_name = None
+        self.default = value
 
-    def attach(self, cls, name):
+    def _attach(self, cls, name):
         self.name = name
+        self.value_name = '_{}'.format(name)
+        setattr(cls, self.value_name, self.default)
 
+    def __get__(self, obj, cls):
+        return self if obj is None else getattr(obj, self.value_name)
+
+    def __set__(self, obj, v):
+        setattr(obj.__class__, self.value_name, v)
+        
 
 class BaseMeta(type):
     __container__ = None
     def __new__(cls, name, bases, attrs):
         pp(locals()); print
+        attrs = self.__process_attrs__(name, bases, attrs)
         c = super(BaseMeta, cls).__new__(cls, name, bases, attrs)
         return c
 
     def __init__(self, name, bases, attrs):
         super(BaseMeta, self).__init__(name, bases, attrs)
 
-        self.__process_attrs__()
-
         if self.__container__:
             self.__container__.register(self)
 
-    def __process_attrs__(self):
-        for k, v in self.__dict__.items():
+        self._ = self()
+
+    def __process_attrs__(self, name, bases, attrs):
+        res_attrs = {}
+        for k, v in attrs:
             if isinstance(v, Attribute):
-                v.attach(cls=self, name=k)
-    
+                res_attrs[k] = v
+                v._attach(cls=self, name=k)                
+            elif not k.startswith('_'):
+                attr = None
+                for b in bases:
+                    if hasattr(k, b):
+                        attr = getattr(k, b)
+                        break
+                if attr:
+                    pass # todo: make
+                    
+                
+            else:
+                res_attrs[k] = v
+
 
 class Container(object):
     def __init__(self):
@@ -71,5 +96,5 @@ class A(root.BaseClass):
 class B(A):
     y = Attribute()
 
-
-    
+a = A()
+b = B()
