39f2bf8d729f56939cab575583207eaf59a6491d
svp <unknown>
2015-04-27 01:29:31 +0300

    things: attrs typization (DEBUG)

--- sublayers_server/model/game_class.py
+++ sublayers_server/model/game_class.py
@@ -6,46 +6,122 @@ log = logging.getLogger(__name__)
 from pprint import pprint as pp
 
 
-class Attribute(object):
-    def __init__(self, value=None):
-        self.name = None
-        self.store_name = None
-        self.default = value
+class AttrError(Exception):
+    """Abstract attribute error"""
 
-    def _attach(self, cls, name):
+
+class AttrValidationError(AttrError):
+    """Attribute value validation error"""
+
+
+class AttrTypeError(AttrValidationError):
+    """Attribute value type error"""
+
+
+class AttrValueIsNull(AttrValidationError):
+    """Not nullable attribute value is None"""
+
+
+class AttrValueRangeError(AttrValidationError):
+    """Attribute range is missing"""
+
+
+class Attribute(object):
+    def __init__(self, default=None, null=True, caption=None, doc=None):
+        self.name = None
+        self.default = default
+        self.null = null
+        self.validate(default)
+        self.doc = doc
+        self._caption = caption
+
+    @property
+    def caption(self):
+        return self._caption or self.name
+
+    @property
+    def value_name(self):
+        return '_{}'.format(self.name)
+
+    def attach(self, cls, name):
         self.name = name
-        self.value_name = '_{}'.format(name)
         setattr(cls, self.value_name, self.default)
 
     def __get__(self, obj, cls):
         return self if obj is None else getattr(obj, self.value_name)
 
     def __set__(self, obj, v):
+        self.validate(v)
         setattr(obj.__class__, self.value_name, v)
-        
+
+    def validate(self, value):
+        if not self.null and value is None:
+            raise AttrValueIsNull()
+
+
+class DocStringAttr(Attribute):
+    
+    @property
+    def value_name(self):
+        return '__doc__'
+
+    def __get__(self, obj, cls):
+        return self if obj is None else getattr(cls, self.value_name)
+
+    def __set__(self, obj, v):
+        setattr(obj.__class__, self.value_name, v)
+    
+
+class StrAttr(Attribute):
+    def validate(self, value):
+        if value is None:
+            super(StrAttr, self).validate(value)
+
+        if not isinstance(value, basestring):
+            raise AttrTypeError('Value type ({}) incompatible. basestring required'.format(type(value)))
+
+
+class NumAttr(Attribute):
+    def __init__(self, default=None, min_value=None, max_value=None, **kw):
+        super(NumAttr, self).__init__(default=default, **kw)
+        self.min_value = min_value
+        self.max_value = max_value
+
+    def validate(self, value):
+        if value is None:
+            super(NumAttr, self).validate(value)
+
+        if self.min_value is not None and value < self.min_value:
+            raise AttrValueRangeError('Value ({}) < of minimal ({})'.format(value, self.min_value))
+
+        if self.max_value is not None and value > self.max_value:
+            raise AttrValueRangeError('Value ({}) > of maximal ({})'.format(value, self.max_value))
+
 
 class BaseMeta(type):
     __container__ = None
-    def __new__(cls, name, bases, attrs):
-        pp(locals()); print
-        c = super(BaseMeta, cls).__new__(cls, name, bases, attrs)
+
+    def __new__(mcs, name, bases, attrs):
+        pp(locals())
+        print
+        c = super(BaseMeta, mcs).__new__(mcs, name, bases, attrs)
         return c
 
-    def __init__(self, name, bases, attrs):
-        super(BaseMeta, self).__init__(name, bases, attrs)
+    def __init__(cls, name, bases, attrs):
+        super(BaseMeta, cls).__init__(name, bases, attrs)
 
-        self._ = self()
+        cls._ = cls()
 
-        self.__process_attrs__()
+        cls.__process_attrs__()
 
-        if self.__container__:
-            self.__container__.register(self)
+        if cls.__container__:
+            cls.__container__.register(cls)
 
     def __process_attrs__(self):
         overrides = {}
         for k, v in self.__dict__.items():
             if isinstance(v, Attribute):
-                v._attach(cls=self, name=k)                
+                v.attach(cls=self, name=k)
             elif (
                 not k.startswith('_')
                 and hasattr(super(self, self), k)
@@ -73,7 +149,7 @@ class Container(object):
         self.__dict__[cls.__name__] = cls
 
     def unregister(self, cls):
-        if isinstance(cls, AbstractBaseClass):
+        if isinstance(cls, BaseClass):
             del self.classes[cls.__name__]
             del self.__dict__[cls.__name__]
         else:
@@ -89,7 +165,7 @@ class BaseClass(object):
     __container__ = root
 
 
-class A(root.BaseClass):
+class A(BaseClass):
     x = Attribute(3)
         
 
--- sublayers_server/model/things.py
+++ sublayers_server/model/things.py
@@ -5,16 +5,21 @@ log = logging.getLogger(__name__)
 
 from pprint import pprint as pp
 
-from sublayers_server.model.game_class import Attribute, Container, BaseClass
+#from sublayers_server.model.
+from game_class import Container, BaseClass, StrAttr, NumAttr
 
 
 class BaseThing(BaseClass):
     u"""Базовый класс всех игровых вещей"""
+    name    = StrAttr('_Thing', caption=u'Имя', doc=u'Имя класса. Должно быть идентификатором.')
+    caption = StrAttr('_Thing', caption=u'Название', doc=u'Человекопонятное название класса.')
+    
+    
 
 
 class Item(BaseThing):
     u"""Базовый класс всех инвентарных предметов"""
-    stack_size = Attribute(1)
+    stack_size = NumAttr(1)
 
 
 class Vehicle(BaseThing):
