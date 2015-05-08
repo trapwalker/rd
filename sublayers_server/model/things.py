800e9350049f9a68d4564e7139932bae61ece623
svp <unknown>
2015-05-08 15:25:11 +0300

    things: add some classes of base ierarchy

--- sublayers_server/model/things.py
+++ sublayers_server/model/things.py
@@ -3,7 +3,11 @@
 import logging
 log = logging.getLogger(__name__)
 
-from pprint import pprint as pp
+if __name__ == '__main__':
+    import sys
+    log.level = logging.DEBUG
+    log.addHandler(logging.StreamHandler(sys.stderr))
+
 
 #from sublayers_server.model.
 from game_class import Container, BaseClass, StrAttr, NumAttr
@@ -15,12 +19,27 @@ class BaseThing(BaseClass):
     caption = StrAttr('_Thing', caption=u'Название', doc=u'Человекопонятное название класса.')
     
     
+class Weapon(BaseThing):
+    u"""Базовый класс оружия"""
+    name = '_Weapon'
+
+
+class MachineGun(Weapon):
+    name = '_MachineGun'
+    dps = NumAttr(default=1, caption=u'DPS', doc=u'Урон в секунду')
+
+
+class VolleyGun(Weapon):
+    name = '_VolleyGun'
+    damage = NumAttr(default=10, caption=u'Урон', doc=u'Урон при стопроцентном попадании')
 
 
 class Item(BaseThing):
     u"""Базовый класс всех инвентарных предметов"""
+    name = '_Item'
     stack_size = NumAttr(1)
 
 
 class Vehicle(BaseThing):
     u"""Базовый класс всех подвижных юнитов"""
+    name = '_Vehicle'
