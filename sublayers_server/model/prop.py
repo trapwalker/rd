f433893498c452c909432ae1821567ab74e5e966
svp <unknown>
2015-06-19 00:10:53 +0300

    things: rearch

--- sublayers_server/model/prop.py
+++ sublayers_server/model/prop.py
@@ -3,13 +3,22 @@
 import logging
 log = logging.getLogger(__name__)
 
-import copy_reg
-from collections import Callable
-from pprint import pprint as pp, pformat
+import sys
 
+if __name__ == '__main__':
+    log.level = logging.DEBUG
+    log.addHandler(logging.StreamHandler(sys.stderr))
+
+#import copy_reg
+#from collections import Callable
+from pprint import pformat
+
+
+# todo: override attributes in subclasses
 
 class Attribute(object):
     def __init__(self, default=None, doc=None, caption=None):
+        # todo: add param: null
         self.name = None
         self.cls = None
         self.default = default
@@ -21,30 +30,38 @@ class Attribute(object):
         assert self.name, 'Attribute is not attached'
         return self.caption or self.name
 
-    @property
-    def raw_name(self):
-        return '_{}'.format(self.name)
-
     def __str__(self):
         return '{self.__class__.__name__}(name={self.name}, cls={self.cls})'.format(self=self)
 
     def __get__(self, obj, cls):
-        value = getattr(obj, self.raw_name, self.default)  # todo: cascade getter
+        value = obj._get_attr_value(self.name, self.default)  # todo: cascade getter
         log.debug('__get__ %s.%s() => %s', obj, self.name, value)
         return value
 
     def __set__(self, obj, value):
         log.debug('__set__ %s.%s = %s', obj, self.name, value)
-        setattr(obj, self.raw_name, value)
+        obj._set_attr_value(self.name, value)
 
     def __delete__(self, obj):
         log.debug('__gelete__ %s.%s() => %s', obj, self.name)
-        delattr(obj, self.raw_name)
+        obj._del_attr_value(self.name)
 
     def attach(self, name, cls):
         self.name = name
         self.cls = cls
-        # tpdp: global attribute registration
+        # todo: global attribute registration
+
+# todo: reserved attr names checking
+
+
+class DocAttribute(Attribute):
+    def __init__(self):
+        super(DocAttribute, self).__init__(caption=u'Описание', doc=u'Описание узла')
+
+    def __get__(self, obj, cls):
+        default = cls.__doc__
+        value = obj._get_attr_value(self.name, self.default or default)  # todo: cascade getter
+        return value
 
 
 class PersistentMeta(type):
@@ -58,62 +75,62 @@ class PersistentMeta(type):
 
     def __init__(cls, name, bases, attrs):
         super(PersistentMeta, cls).__init__(name, bases, attrs)
-        cls.__process_attrs__()
+        # cls.__process_attrs__()
 
     def __process_attrs__(self):
-        overrides = {}
         for k, v in self.__dict__.items():
             if isinstance(v, Attribute):
                 v.attach(name=k, cls=self)
-            elif (
-                not k.startswith('_')
-                and hasattr(super(self, self), k)
-                and isinstance(getattr(super(self, self), k), Attribute)
-                and not isinstance(v, Attribute)
-            ):
-                overrides[k] = v
-
-        for k, v in overrides.items():
-            log.debug('override %s=%s', k, v)
-            delattr(self, k)  # todo: фильтровать перекрытия до инстанцирования вместо удаления после
-            setattr(self._, k, v)
 
 
 class Persistent(object):
-    pass
+    __metaclass__ = PersistentMeta
 
 
-class Node(object):
-    def __init__(self, name, owner=None, parent=None, attrs=None):
-        self._owner = None
+class Node(Persistent):
+    abstract = Attribute(default=True, caption=u'Абстракция', doc=u'Признак абстрактности узла')
+    doc = DocAttribute()
+
+    def __init__(self, name, parent=None, abstract=False, values=None,  **kw):
         self.name = name
         self.parent = parent
-        self.attrs = attrs or {}
-        self.owner = owner
+        self.abstract = abstract
+        self.values = values or {}
+        self.values.update(kw)
+        self.childs = []  # todo: use weakref
+        if parent:
+            parent._add_child(self)
 
-    @property
-    def path(self):
-        return (self.owner.path if self.owner else ()) + (self.name)
+    def _add_child(self, child):
+        self.childs.append(child)
 
-    @property
-    def fullname(self):
-        return '.'.join(self.path)
-
-    def __getattr__(self, item):
-        if item in self.attrs:
-            return self.attrs[item]
+    def _get_attr_value(self, name, default):
+        if name in self.values:
+            return self.values[name]
         if self.parent:
-            return getattr(self.parent, item)
+            return self.parent._get_attr_value(name, default)
         else:
-            raise AttributeError('{} has no attribute {} in parent line'.format(self.fullname, item))
+            return default
+
+    def _set_attr_value(self, name, value):
+        self.values[name] = value
+
+    def _del_attr_value(self, name):
+        del(self.values[name])
+
+    def _has_attr_value(self, name):
+        return name in self.values
 
 
 if __name__ == '__main__':
-    import sys
-    log.level = logging.DEBUG
-    log.addHandler(logging.StreamHandler(sys.stderr))
+    # from pickle import dumps, loads
+    # import jsonpickle as jp
+    class Car(Node):
+        u"""Абстрактная машина"""
+        max_velocity = Attribute(default=100, caption=u'Макс. скорость', doc=u'Максимальная скорость транспортного средства')
 
-    from pickle import dumps, loads
-    import jsonpickle as jp
-    
-    
+    anyCar = Car(name='AnyCar')
+    carLite = Car(name='CarLite', parent=anyCar, doc=u'Лёгкая техника (мото-, вело-, скейт, коньки, тапки)')
+    carMidle = Car(name='CarMiddle', parent=anyCar, doc=u'Легковая техника')
+    carHavy = Car(name='CarHavy', parent=anyCar, doc=u'Транспорт тяжелго класса (грузовики, тягачи, танки')
+
