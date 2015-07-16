142f57c7ef6450cd0f90a20db2ba5c296809c602
menkent <unknown>
2015-07-16 11:53:42 +0300

    server: вернул старые конфигурации машинок

--- sublayers_server/model/first_mission_parties.py
+++ sublayers_server/model/first_mission_parties.py
@@ -12,25 +12,25 @@ class RandomCarList:
     def __init__(self):
         self.params_cars = []
         self.base_point = Point(12496376, 27133643)
-        # self.params_cars.append(
-        #     dict(
-        #         max_hp=100,
-        #         r_min=14.0,
-        #         ac_max=14.0,
-        #         v_forward=(80 * 1000 / 3600),
-        #         v_backward=(-40 * 1000 / 3600),
-        #         a_forward=4.0,
-        #         a_backward=-3.0,
-        #         a_braking=-6.0,
-        #         observing_range=270,
-        #         weapons=[
-        #             dict(fi=0, is_auto=False, radius=200, width=radians(40), dmg=10, time_recharge=5),
-        #             dict(fi=0, is_auto=False, radius=50, width=radians(60), dmg=2, time_recharge=10),
-        #             dict(fi=pi / 2, is_auto=True, radius=200, width=radians(30), dps=0.5),
-        #             dict(fi=-pi / 2, is_auto=True, radius=200, width=radians(30), dps=0.5),
-        #         ],
-        #     )
-        # )
+        self.params_cars.append(
+            dict(
+                max_hp=100,
+                r_min=14.0,
+                ac_max=14.0,
+                v_forward=(80 * 1000 / 3600),
+                v_backward=(-40 * 1000 / 3600),
+                a_forward=4.0,
+                a_backward=-3.0,
+                a_braking=-6.0,
+                observing_range=270,
+                weapons=[
+                    dict(fi=0, is_auto=False, radius=200, width=radians(40), dmg=10, time_recharge=5),
+                    dict(fi=0, is_auto=False, radius=50, width=radians(60), dmg=2, time_recharge=10),
+                    dict(fi=pi / 2, is_auto=True, radius=200, width=radians(30), dps=0.5),
+                    dict(fi=-pi / 2, is_auto=True, radius=200, width=radians(30), dps=0.5),
+                ],
+            )
+        )
 
         self.params_cars.append(
             dict(
@@ -44,8 +44,8 @@ class RandomCarList:
                 a_braking=-8.9,
                 observing_range=300,
                 weapons=[
-                    # dict(fi=0, is_auto=False, radius=150, width=radians(40), dmg=5, time_recharge=3),
-                    # dict(fi=0, is_auto=False, radius=50, width=radians(60), dmg=2, time_recharge=10),
+                    dict(fi=0, is_auto=False, radius=150, width=radians(40), dmg=5, time_recharge=3),
+                    dict(fi=0, is_auto=False, radius=50, width=radians(60), dmg=2, time_recharge=10),
                     dict(fi=0, is_auto=True, radius=300, width=radians(30), dps=0.8),
                 ],
             )
