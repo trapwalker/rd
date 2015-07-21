d49ecb2f7d2a064079432487cadbd12b7f3b5121
abbir <unknown>
2015-07-21 16:50:24 +0300

    server: поиск бага стрельбы.

--- sublayers_server/model/weapons.py
+++ sublayers_server/model/weapons.py
@@ -79,12 +79,14 @@ class WeaponAuto(Weapon):
 
     def _start_fire_to_car(self, car, time):
         car.set_hp(dps=self.dps, add_shooter=self.owner, time=time)
+        self.targets.append(car)
         for agent in self.owner.subscribed_agents:
             FireAutoEffect(agent=agent, subj=self.owner, obj=car, side=self.sector.side, action=True, time=time).post()
 
     def _stop_fire_to_car(self, car, time):
         if not car.is_died(time=time):  # если цель мертва, то нет смысла снимать с неё дамаг
             car.set_hp(dps=-self.dps, del_shooter=self.owner, time=time)
+        self.targets.remove(car)
         for agent in self.owner.subscribed_agents:
             FireAutoEffect(agent=agent, subj=self.owner, obj=car, side=self.sector.side, action=False, time=time).post()
 
