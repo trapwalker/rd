2b103be26e2a830c7d12974416c3d8919eff060e
abbir <unknown>
2015-07-22 10:52:29 +0300

    server: баг фикс:         + добавили обработку отсутствия профиля игрока в бд         + ник ограничен 20 символами.

--- sublayers_server/handlers/client_connector.py
+++ sublayers_server/handlers/client_connector.py
@@ -24,39 +24,35 @@ class AgentSocketHandler(tornado.websocket.WebSocketHandler):
     def open(self):
         # todo: make agent_init event
         self.user_id = self.get_secure_cookie("user")
+        self.agent = None
         log.info('!!! Open User connection: %s', self.user_id)
+        self.application.clients.append(self)
         # log.debug('Cookies: %s', self.cookies)
         srv = self.application.srv
         agent = srv.api.get_agent(self.user_id, make=True, do_disconnect=True)  # todo: Change to make=False
+        if agent is None:
+            log.warn('Agent not found in database')
+            return
         log.info('Agent %r socket Init', self)
         self.agent = agent
         agent.connection = self
-        self.application.clients.append(self)
         if agent.api:
             self.api = agent.api
             agent.api.update_agent_api()
         else:
             self.api = AgentAPI(agent=agent)
 
-        # todo: extract chat logic from here
-        for message_params in self.application.chat:
-            srv.post_message(messages.Chat(agent=agent, **message_params))
-            # todo: otimize - slice and send chat history by chunks
-
         # обновление статистики по онлайну агентов
         srv.stat_log.s_agents_on(time=srv.get_time(), delta=1.0)
 
     def on_close(self):
-        # todo: removing car from worldmap by disconnect
-        log.info('Agent %r socket Closed', self)
-        self.agent.on_disconnect(self)
+        if self.agent:
+            log.info('Agent socket %r Closed', self)
+            self.agent.on_disconnect(self)
+        else:
+            log.warn('Socket Closed. Socket %r has not agent', self)
         self.application.clients.remove(self)
 
-        # while self.agent.cars:
-        #     car = self.agent.cars[0]
-        #     if car.is_alive:
-        #         self.agent.drop_car(car)
-        #         car.delete()
 
     def on_message(self, message):
         # log.debug("Got message from %s: %r", self.agent, message)
