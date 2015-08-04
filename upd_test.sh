af9e2a87376aa873ce6348fcc3c2565ffbc6f7a5
svp <unknown>
2015-08-04 23:48:32 +0300

    deploy: upd script changed

--- upd_test.sh
+++ upd_test.sh
@@ -14,10 +14,17 @@ REV1w=`hg identify sublayers_server/world | awk '{print $1}'`
 hg pull
 hg update @BRANCHE
 
+cd world
+
+hg pull
+hg update @BRANCHE
+
+cd ..
+
 REV2=`hg identify | awk '{print $1}'`
 REV2w==`hg identify sublayers_server/world | awk '{print $1}'`
 
-[ "$REV1" = "$REV2" ] || echo 'Server source changed'
-[ "$REV1w" = "$REV2w" ] || echo 'Server world changed'
+[ "$REV1" = "$REV2" ] || echo "Server source changed: $REV1 != $REV2"
+[ "$REV1w" = "$REV2w" ] || echo "Server world changed: $REV1w != $REV2w"
 
 [ "$REV1" = "$REV2" ] && [ "$REV1w" = "$REV2w" ] && echo 'no changes' || service sl$SUFFIX restart
