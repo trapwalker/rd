8beb2b5235c3fd9f3da8bdf3457497102ec08809
svp <unknown>
2015-08-05 00:08:15 +0300

    deploy: update script fixed (2)

--- upd_test.sh
+++ upd_test.sh
@@ -11,18 +11,25 @@ cd /home/sl/sublayers$SUFFIX
 REV1=`hg identify | awk '{print $1}'`
 REV1w=`hg identify sublayers_server/world | awk '{print $1}'`
 
-hg pull
-hg update @BRANCHE
-
-cd world
+echo ---- `pwd` [$REV1] [$REV1w]
 
 hg pull
 hg update @BRANCHE
 
-cd ..
+cd sublayers_server/world
+
+echo ---- `pwd`
+
+hg pull
+hg update @BRANCHE
+
+cd ../..
+
 
 REV2=`hg identify | awk '{print $1}'`
-REV2w==`hg identify sublayers_server/world | awk '{print $1}'`
+REV2w=`hg identify sublayers_server/world | awk '{print $1}'`
+
+echo `pwd` [$REV2] [$REV2w]
 
 [ "$REV1" = "$REV2" ] || echo "Server source changed: $REV1 != $REV2"
 [ "$REV1w" = "$REV2w" ] || echo "Server world changed: $REV1w != $REV2w"
