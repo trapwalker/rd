8307bee81b4f78e03957cef3c65a13580a50be67
svp <unknown>
2015-10-02 16:36:20 +0300

    model/quest: quest class init

---
+++ sublayers_server/model/registry/classes/quests.py
@@ -0,0 +1,12 @@
+﻿# -*- coding: utf-8 -*-
+
+import logging
+log = logging.getLogger(__name__)
+
+from sublayers_server.model.registry.classes.item import Item
+from sublayers_server.model.registry.attr.link import RegistryLink
+from sublayers_server.model.registry.attr import Attribute, FloatAttribute, TextAttribute
+
+
+class Quest(Item):
+    pass
