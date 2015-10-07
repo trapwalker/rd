2b290be02efd2461077994065af26969fa3641a1
svp <unknown>
2015-10-07 13:12:56 +0300

    model/reg/exptable: ExpTable registry class init

---
+++ sublayers_server/model/registry/classes/exptable.py
@@ -0,0 +1,11 @@
+﻿# -*- coding: utf-8 -*-
+
+import logging
+log = logging.getLogger(__name__)
+
+from sublayers_server.model.registry.storage import Root
+from sublayers_server.model.registry.attr import Attribute
+
+
+class ExpTable(Root):
+    table = Attribute(caption=u'Узловые точки', doc=u'Таблица узловых точек шкалы опыта')
