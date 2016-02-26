627abf5288c2f4ec9a7abede0d100a293158239e
svp <unknown>
2016-02-26 11:12:54 +0300

    deploy: service scripts refactoring

--- upd.sh
+++ upd.sh
@@ -1,37 +1,27 @@
 #! /bin/sh
 
-BRANCHE=release
+BRANCHE=''
 SUFFIX=''
 
+PTH=/home/sl/sublayers$SUFFIX
+PTHW=$PTH/sublayers_server/world
 
-echo '------------------------------------'
-echo `date --rfc-3339=seconds`
-cd /home/sl/sublayers$SUFFIX
+echo `date --rfc-3339=seconds` Start update of $PTH
 
-REV1=`hg identify | awk '{print $1}'`
-REV1w=`hg identify sublayers_server/world | awk '{print $1}'`
+REV1=`hg identify -R $PTH | awk '{print $1}'`
+REV1w=`hg identify -R $PTHW | awk '{print $1}'`
+echo `date --rfc-3339=seconds` repo: $PTH revisions: [$REV1] [$REV1w]
 
-echo ---- `pwd` [$REV1] [$REV1w]
+hg pull -R $PTH
+hg update $BRANCHE -R $PTH
 
-hg pull
-hg update $BRANCHE
+hg pull -R $PTHW
+hg update $BRANCHE -R $PTHW
 
-cd sublayers_server/world
-
-echo ---- `pwd`
-
-hg pull
-hg update $BRANCHE
-
-cd ../..
-
-
-REV2=`hg identify | awk '{print $1}'`
-REV2w=`hg identify sublayers_server/world | awk '{print $1}'`
-
-echo `pwd` [$REV2] [$REV2w]
+REV2=`hg identify -R $PTH | awk '{print $1}'`
+REV2w=`hg identify -R $PTHW | awk '{print $1}'`
+echo `date --rfc-3339=seconds` repo: $PTH new_revisions: [$REV2] [$REV2w]
 
 [ "$REV1" = "$REV2" ] || echo "Server source changed: $REV1 != $REV2"
 [ "$REV1w" = "$REV2w" ] || echo "Server world changed: $REV1w != $REV2w"
-
 [ "$REV1" = "$REV2" ] && [ "$REV1w" = "$REV2w" ] && echo 'no changes' || service sl$SUFFIX restart
