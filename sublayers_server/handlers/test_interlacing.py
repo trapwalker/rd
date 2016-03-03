fb04f961f513d947cc183b6342d9c89ed85b1225
menkent <unknown>
2016-03-03 14:47:17 +0300

    server/client: interlacing

---
+++ sublayers_server/handlers/test_interlacing.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import logging
+log = logging.getLogger(__name__)
+
+import tornado.web
+from tornado.options import options
+
+from .base import BaseHandler
+
+
+class TestInterlacingHandler(BaseHandler):
+    def get(self):
+        brightness = self.get_argument('brightness', '70')
+        if not (0 <= int(brightness) <= 100):
+            brightness = '70'
+        self.render("test_interlacing.html", brightness=brightness)
