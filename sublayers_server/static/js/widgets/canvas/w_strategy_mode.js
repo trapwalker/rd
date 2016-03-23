cb6cf65dbc5a96d8ff7c7230f7474ddcc6a649d6
menkent <unknown>
2016-03-23 16:59:35 +0300

    client: клиентские наработки по стретегическому режиму

---
+++ sublayers_server/static/js/widgets/canvas/w_strategy_mode.js
@@ -0,0 +1,42 @@
+var WStrategyModeManager = (function () {
+    function WStrategyModeManager() {
+        mapCanvasManager.add_vobj(this, 80);
+        this.targets = [];  // —писок точек дл€ отображени€ в стратегическом режиме
+
+    }
+
+    WStrategyModeManager.prototype.update = function (targets) {
+        //console.log('WStrategyModeManager.prototype.update');
+        this.targets = targets;
+    };
+
+
+    WStrategyModeManager.prototype.redraw = function(ctx, time){
+        //console.log('WStrategyModeManager.prototype.redraw');
+        if (mapCanvasManager.real_zoom >= 15) return;
+        ctx.save();
+        if(mapCanvasManager.real_zoom > 14) {
+            ctx.globalAlpha = 15.0 - mapCanvasManager.real_zoom;
+        }
+
+        var map_top_left = mapCanvasManager.map_tl;
+
+        ctx.fillStyle = '#FF0000';
+        // ќтрисовка точек
+        for (var i = 0; i < this.targets; i++){
+            var p = subVector(this.targets[i], map_top_left);
+            // todo: не рисовать точки, которые заведомо никак не попадут на канвас
+            ctx.beginPath();
+            ctx.arc(p.x, p.y, 3, 2 * Math.PI, 0, false);
+            ctx.closePath();
+            ctx.fill();
+        }
+
+        ctx.restore();  // ¬озврат транслейта
+    };
+
+
+    return WStrategyModeManager;
+})();
+
+var wStrategyModeManager;
