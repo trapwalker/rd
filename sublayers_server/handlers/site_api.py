3db76ebfd671712004eec1fb56d87eeda7f39c41
menkent <unknown>
2016-04-07 17:26:57 +0300

    server: генерация машинки через uri, но uri пока не передаётся :(

--- sublayers_server/handlers/site_api.py
+++ sublayers_server/handlers/site_api.py
@@ -9,7 +9,7 @@ from tornado.options import options
 from sublayers_server.user_profile import User
 
 from sublayers_server.handlers.base import BaseHandler
-
+from sublayers_server.model.registry.classes.mobiles import Car as RegCar
 
 
 class APIGetCarInfoHandler(BaseHandler):
@@ -32,17 +32,17 @@ class APIGetCarInfoHandler(BaseHandler):
 
 class APIGetCarInfoHandler2(BaseHandler):
     def get(self):
-        username = self.get_argument('username', None)
         uri = self.get_argument('uri', None)
-        if not username:
+        uri = 'reg://registry/mobiles/cars/middle/sports/delorean_dmc12'
+        if not uri:
             self.send_error(404)
             return
-        agent = self.application.srv.api.get_agent(username)
-        if not agent:
+        ex_car = None
+        try:
+            ex_car = self.application.srv.reg[uri]
+        except:
+            ex_car = None
+        if not ex_car or not isinstance(ex_car, RegCar):
             self.send_error(404)
             return
-        ex_car = agent.example.car
-        if not ex_car:
-            self.send_error(404)
-            return
-        self.render('location/car_info_img_ext.html', car=ex_car)
+        self.render('location/car_info_img_ext_for_clear_web.html', car=ex_car)
