f602660b5fdecd6043602802d113f821cca6b254
menkent <unknown>
2016-04-21 17:37:02 +0300

    site: непосредственно класс эмуляции клавиатурных нажатий

---
+++ sublayers_site/static_site/js/audio_keyboard_emulate.js
@@ -0,0 +1,31 @@
+var AudioKeyboard = (function () {
+    function AudioKeyboard(audio_list) {
+        this.audio_list = audio_list;
+        this.gain = 1.0;
+        this.need_stop = false;
+    }
+
+    // Воспроизведение
+    AudioKeyboard.prototype.play = function () {
+        if (this.need_stop) {
+            this.need_stop = false;
+            return;
+        }
+        var audio_obj = this.audio_list[Math.floor(Math.random() * 0.99 * this.audio_list.length)];
+        audio_obj.play(0, this.gain, AudioKeyboard.prototype.play.bind(this));
+    };
+
+    AudioKeyboard.prototype.stop = function () {
+        this.need_stop = true;
+    };
+
+    // Установка громкости
+    AudioKeyboard.prototype.gain = function (value) {
+        this.gain = value;
+    };
+
+    return AudioKeyboard;
+})();
+
+var audioKeyboard;
+
