31866887ca693304618b6af3fcf6df16579f3deb
abbir <unknown>
2016-05-20 11:21:15 +0300

    client: new town:         + добавлена сущность LocationPlaceBuilding;         + добавлены новые npc (продавец машин и парковщик);         + проброшены клики на кнопки боковых меню.

--- sublayers_common/static/js/location/location_manager.js
+++ sublayers_common/static/js/location/location_manager.js
@@ -27,31 +27,14 @@ var LocationManager = (function () {
     };
 
     LocationManager.prototype.openBuilding = function (buildingName) {
-        console.log('LocationManager.prototype.openBuilding', buildingName);
+        //console.log('LocationManager.prototype.openBuilding', buildingName);
         if (!this.buildings.hasOwnProperty(buildingName)) return;
-
         var building = this.buildings[buildingName];
-        console.log(building);
-        this.screens.location_screen = building;
-
-        // Перейти в новое здание если мы сейчас на экране локации
-        if (this.active_screen_name == 'location_screen')
-            building.activate();
-
-
-        //function openBuilding(building_name) {
-    //    $('.townPageWrap').css('display', 'none');
-    //
-    //    $('#img_grid').css('opacity', '0.5');
-    //    $('#building_' + building_name).css('display', 'block');
-    //    for (var i = 6; i < 10; i ++)
-    //        setBtnDisplay(i, "none");
-    //    $("#btn_5_select").mouseup(function(event) { openTown(); });
-    //}
+        building.activate();
     };
 
     LocationManager.prototype.onEnter = function (data) {
-        console.log('LocationManager.prototype.onEnter', data);
+        //console.log('LocationManager.prototype.onEnter', data);
         this.onExit();
 
         // Закрыть все окна
@@ -68,12 +51,13 @@ var LocationManager = (function () {
         // Создаем окна зданий
         for (var i = 0; i < data.location.example_town.buildings.length; i++) {
             var building_rec = data.location.example_town.buildings[i];
-            console.log(building_rec);
-            this.buildings[building_rec.key] = new LocationPlace(building_rec, this.jq_town_div);
+            this.buildings[building_rec.key] = new LocationPlaceBuilding(building_rec, this.jq_town_div);
         }
         this.active_screen_name = 'location_screen';
 
-        this.setBtnState(3, '</br>Назад', true);
+        this.setBtnState(1, '', false);
+        this.setBtnState(2, '', false);
+        this.setBtnState(3, '</br>Назад', false);
         this.setBtnState(4, '</br>Выход', true);
 
         //locationManager.location_uid = event.location.uid;
@@ -123,7 +107,7 @@ var LocationManager = (function () {
     };
 
     LocationManager.prototype.setBtnState = function (btnIndex, btnText, active) {
-        //console.log('LocationManager.prototype.onExit');
+        //console.log('LocationManager.prototype.setBtnState', btnIndex, btnText, active);
         $('#btn_' + btnIndex + '_text').html(btnText);
         if (active) {
             $('#btn_' + btnIndex + '_noactive').css('display', 'none');
@@ -131,7 +115,18 @@ var LocationManager = (function () {
         } else {
             $('#btn_' + btnIndex + '_noactive').css('display', 'block');
             $('#btn_' + btnIndex + '_active').css('display', 'none');
+
+            $('#btn_' + btnIndex + '_hover').css('display', 'none');
+            $('#btn_' + btnIndex + '_pressed').css('display', 'none');
+            $('#btn_' + btnIndex + '_text').removeClass('hover');
+            $('#btn_' + btnIndex + '_text').removeClass('pressed');
         }
+    };
+
+    LocationManager.prototype.clickBtn = function (btnIndex) {
+        //console.log('LocationManager.prototype.clickBtn', btnIndex);
+        if (this.screens[this.active_screen_name])
+            this.screens[this.active_screen_name].clickBtn(btnIndex);
     };
 
     return LocationManager;
@@ -160,22 +155,35 @@ var LocationPanelInfo = (function () {
 
 
 var LocationPlace = (function () {
-    function LocationPlace(building_rec, jq_main_div) {
-        this.building_rec = building_rec;
-        this.jq_main_div = jq_main_div.find('#building_' + this.building_rec.key);
+    function LocationPlace(jq_main_div, screen_name) {
+        this.jq_main_div = jq_main_div;
+        this.screen_name = screen_name;
     }
 
     LocationPlace.prototype.activate = function () {
         //console.log('LocationPlace.prototype.activate');
 
+        // Выключить ландшафт здания и специалистов
         $('#layer2').css('display', 'none');
         $('#landscape').css('display', 'none');
         $('.building-back').css('display', 'none');
-        $('#' + this.building_rec.key + '-back').css('display', 'block');
 
-        // todo: выключить все места, здания и специалистов
+        // Включить своё окно
         this.jq_main_div.css('display', 'block');
-        locationManager.screens[locationManager.active_screen_name] = this;
+        if (this.screen_name) // если для локации указан конкретный скрин, то записаться в него
+            locationManager.screens[this.screen_name] = this;
+        else // если нет, то записаться в последний активный
+            locationManager.screens[locationManager.active_screen_name] = this;
+
+        // Настроить кнопки
+        this.set_buttons();
+    };
+
+    LocationPlace.prototype.set_buttons = function () {
+        locationManager.setBtnState(1, '', false);
+        locationManager.setBtnState(2, '', false);
+        locationManager.setBtnState(3, '', false);
+        locationManager.setBtnState(4, '', false);
     };
 
     LocationPlace.prototype.clear = function () {
@@ -185,9 +193,58 @@ var LocationPlace = (function () {
         this.jq_main_div = null;
     };
 
+    LocationPlace.prototype.clickBtn = function (btnIndex) {};
+
     return LocationPlace;
 })();
 
+
+var LocationPlaceBuilding = (function (_super) {
+    __extends(LocationPlaceBuilding, _super);
+
+    function LocationPlaceBuilding(building_rec, jq_town_div) {
+        this.building_rec = building_rec;
+        _super.call(this, jq_town_div.find('#building_' + this.building_rec.key), 'location_screen');
+    }
+
+    LocationPlaceBuilding.prototype.activate = function () {
+        //console.log('LocationPlaceBuilding.prototype.activate');
+        _super.prototype.activate.call(this);
+        $('#' + this.building_rec.key + '-back').css('display', 'block');
+    };
+
+    LocationPlaceBuilding.prototype.clickBtn = function (btnIndex) {
+        //console.log('LocationPlaceBuilding.prototype.clickBtn', btnIndex);
+        switch (btnIndex) {
+            case '3':
+                $('#layer2').css('display', 'block');
+                $('#landscape').css('display', 'block');
+                $('.building-back').css('display', 'none');
+                this.jq_main_div.css('display', 'none');
+                if (this.screen_name) // если для локации указан конкретный скрин, то записаться в него
+                    locationManager.screens[this.screen_name] = null;
+                else // если нет, то записаться в последний активный
+                    locationManager.screens[locationManager.active_screen_name] = null;
+
+                locationManager.setBtnState(1, '', false);
+                locationManager.setBtnState(2, '', false);
+                locationManager.setBtnState(3, '</br>Назад', false);
+                locationManager.setBtnState(4, '</br>Выход', true);
+
+                break;
+            case '4':
+                console.log('выход из города');
+                break;
+        }
+    };
+
+    LocationPlaceBuilding.prototype.set_buttons = function () {
+        locationManager.setBtnState(3, '</br>Назад', true);
+        locationManager.setBtnState(4, '</br>Выход', true);
+    };
+
+    return LocationPlaceBuilding;
+})(LocationPlace);
 
 var locationManager;
 
