9beb4d9f90c13a96f888e556304c2efb0b0c6cad
svp <unknown>
2016-06-16 17:23:08 +0300

    reg/classes/agents: new ODM migration INDEV

--- sublayers_server/model/registry/classes/agents.py
+++ sublayers_server/model/registry/classes/agents.py
@@ -5,9 +5,9 @@ log = logging.getLogger(__name__)
 
 
 from sublayers_server.model.registry.storage import Root
+from sublayers_server.model.registry.odm_position import PositionField
 from sublayers_server.model.registry.odm.fields import (
-    StringField, ListField, BooleanField, UUIDField,
-    UniReferenceField,
+    FloatField, StringField, ListField, UniReferenceField, EmbeddedDocumentField
 )
 
 
@@ -24,13 +24,11 @@ class Agent(Root):
         default=[], caption=u"Список всех машин, кроме активной",
     )
 
-    position = Position(caption=u"Последние координаты агента")
-    balance = FloatAttribute(default=1000, caption=u"Количество литров на счете агента", tags='client')  # todo: обсудить
+    position = PositionField(caption=u"Последние координаты агента")
+    balance = FloatField(default=1000, caption=u"Количество литров на счете агента", tags='client')  # todo: обсудить
 
-    last_town = RegistryLink(caption=u"Последний посещенный город")
-    current_location = RegistryLink(caption=u"Текущая локация")
-
-    # todo: current location link
+    last_town = UniReferenceField(caption=u"Последний посещенный город")
+    current_location = UniReferenceField(caption=u"Текущая локация")
 
     # todo: party link
     # todo: invites list
@@ -40,28 +38,46 @@ class Agent(Root):
     perks = InventoryPerksAttribute(caption=u'Список прокачанных перков')
 
     # Механизм скилов
-    exp_table = RegistryLink(default='reg://registry/rpg_settings/exptable', caption=u"Таблица опыта")
-    role_class = RegistryLink(caption=u"Ролевой класс")
+    exp_table = UniReferenceField(default='reg://registry/rpg_settings/exptable', caption=u"Таблица опыта")
+    role_class = UniReferenceField(caption=u"Ролевой класс")
 
-    buy_driving = RegistryLink(default='reg://registry/rpg_settings/buy_skill',
-                               caption=u"Купленные очки навыка вождения")
-    buy_shooting = RegistryLink(default='reg://registry/rpg_settings/buy_skill',
-                                caption=u"Купленные очки навыка стрельбы")
-    buy_masking = RegistryLink(default='reg://registry/rpg_settings/buy_skill',
-                               caption=u"Купленные очки навыка маскировки")
-    buy_leading = RegistryLink(default='reg://registry/rpg_settings/buy_skill',
-                               caption=u"Купленные очки навыка лидерства")
-    buy_trading = RegistryLink(default='reg://registry/rpg_settings/buy_skill',
-                               caption=u"Купленные очки навыка торговли")
-    buy_engineering = RegistryLink(default='reg://registry/rpg_settings/buy_skill',
-                                   caption=u"Купленные очки навыка инженеринга")
+    buy_driving = UniReferenceField(
+        default='reg://registry/rpg_settings/buy_skill',
+        caption=u"Купленные очки навыка вождения",
+        reference_document_type='sublayers_server.model.registry.classes.skills.BuySkill',
+    )
+    buy_shooting = UniReferenceField(
+        default='reg://registry/rpg_settings/buy_skill',
+        caption=u"Купленные очки навыка стрельбы",
+        reference_document_type='sublayers_server.model.registry.classes.skills.BuySkill',
+    )
+    buy_masking = UniReferenceField(
+        default='reg://registry/rpg_settings/buy_skill',
+        caption=u"Купленные очки навыка маскировки",
+        reference_document_type='sublayers_server.model.registry.classes.skills.BuySkill',
+    )
+    buy_leading = UniReferenceField(
+        default='reg://registry/rpg_settings/buy_skill',
+        caption=u"Купленные очки навыка лидерства",
+        reference_document_type='sublayers_server.model.registry.classes.skills.BuySkill',
+    )
+    buy_trading = UniReferenceField(
+        default='reg://registry/rpg_settings/buy_skill',
+        caption=u"Купленные очки навыка торговли",
+        reference_document_type='sublayers_server.model.registry.classes.skills.BuySkill',
+    )
+    buy_engineering = UniReferenceField(
+        default='reg://registry/rpg_settings/buy_skill',
+        caption=u"Купленные очки навыка инженеринга",
+        reference_document_type='sublayers_server.model.registry.classes.skills.BuySkill',
+    )
 
-    driving = RegistryLink(default='reg://registry/rpg_settings/skill', caption=u"Навык вождения", tags='skill')
-    shooting = RegistryLink(default='reg://registry/rpg_settings/skill', caption=u"Навык стрельбы", tags='skill')
-    masking = RegistryLink(default='reg://registry/rpg_settings/skill', caption=u"Навык маскировки", tags='skill')
-    leading = RegistryLink(default='reg://registry/rpg_settings/skill', caption=u"Навык лидерства", tags='skill')
-    trading = RegistryLink(default='reg://registry/rpg_settings/skill', caption=u"Навык торговли", tags='skill')
-    engineering = RegistryLink(default='reg://registry/rpg_settings/skill', caption=u"Навык инженеринга", tags='skill')
+    driving = UniReferenceField(default='reg://registry/rpg_settings/skill', caption=u"Навык вождения", tags='skill')
+    shooting = UniReferenceField(default='reg://registry/rpg_settings/skill', caption=u"Навык стрельбы", tags='skill')
+    masking = UniReferenceField(default='reg://registry/rpg_settings/skill', caption=u"Навык маскировки", tags='skill')
+    leading = UniReferenceField(default='reg://registry/rpg_settings/skill', caption=u"Навык лидерства", tags='skill')
+    trading = UniReferenceField(default='reg://registry/rpg_settings/skill', caption=u"Навык торговли", tags='skill')
+    engineering = UniReferenceField(default='reg://registry/rpg_settings/skill', caption=u"Навык инженеринга", tags='skill')
 
     def iter_skills(self):  # todo: need review
         for name, attr, getter in self.iter_attrs(tags='skill'):
@@ -72,7 +88,14 @@ class Agent(Root):
             yield perk
             
     def skill_point_summ(self):
-        return self.driving.value + self.shooting.value + self.masking.value + self.leading.value + self.trading.value + self.engineering.value
+        return (
+            self.driving.value +
+            self.shooting.value +
+            self.masking.value +
+            self.leading.value +
+            self.trading.value +
+            self.engineering.value
+        )
 
     def as_client_dict(self):
         d = super(Agent, self).as_client_dict()
