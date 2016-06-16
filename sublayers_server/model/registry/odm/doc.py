055b7bd6960b4d2b8f2b64ef2ef0eb9157812521
svp <unknown>
2016-06-16 17:21:08 +0300

    reg/odm: Поддержка метода set_value у полей

--- sublayers_server/model/registry/odm/doc.py
+++ sublayers_server/model/registry/odm/doc.py
@@ -49,9 +49,50 @@ class AbstractDocument(Document):
     def get_global_class_name(cls):
         return cls.__name__  # todo: add module importing path
 
-    def __init__(self, **kw):
+    # def __init__(self, **kw):
+    #     kw.setdefault('__cls__', self.get_global_class_name())
+    #     #super(AbstractDocument, self).__init__(**kw)
+
+    def __init__(self, _is_partly_loaded=False, _reference_loaded_fields=None, **kw):
+        """
+        :param _is_partly_loaded: is a flag that indicates if the document was
+        loaded partly (with `only`, `exlude`, `fields`). Default: False.
+        :param _reference_loaded_fields: dict that contains projections for
+        reference fields if any. Default: None.
+        :param kw: pairs of fields of the document and their values
+        """  # полностью перекрыто для реализации field.set_value
+        from motorengine.fields.dynamic_field import DynamicField
         kw.setdefault('__cls__', self.get_global_class_name())
-        super(AbstractDocument, self).__init__(**kw)
+
+        self._id = kw.pop('_id', None)
+        self._values = {}
+        self.is_partly_loaded = _is_partly_loaded
+
+        if _reference_loaded_fields:
+            self._reference_loaded_fields = _reference_loaded_fields
+        else:
+            self._reference_loaded_fields = {}
+
+        for key, field in self._fields.items():
+            if callable(field.default):
+                self._values[field.name] = field.set_value(field.default())
+            else:
+                self._values[field.name] = field.set_value(field.default)
+
+        for key, value in kw.items():
+            field = self._fields.get(key)
+            if field:
+                value = field.set_value(value)
+            else:
+                self._fields[key] = DynamicField(db_field="_%s" % key.lstrip('_'))
+            self._values[key] = value
+
+    def __setattr__(self, name, value):
+        if name in self._fields:
+            field = self._fields[name]
+            self._values[name] = field.set_value(value)
+        else:
+            super(AbstractDocument, self).__setattr__(name, value)
 
     @classmethod
     def from_son(cls, dic, _is_partly_loaded=False, _reference_loaded_fields=None):
