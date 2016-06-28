af0e0a8594afc586362bef24c5f627f80d848ebd
svp <unknown>
2016-06-28 11:05:14 +0300

    reg/classes/skills: Миграция на ODM. Изменена структура таблицы цен - теперь таблица -- это список, а не словарь - todo: Исправить клиент.

--- sublayers_server/model/registry/classes/skills.py
+++ sublayers_server/model/registry/classes/skills.py
@@ -6,23 +6,27 @@ log = logging.getLogger(__name__)
 import math
 
 from sublayers_server.model.registry.storage import Root
-from sublayers_server.model.registry.attr.link import RegistryLink
-from sublayers_server.model.registry.attr import Attribute, TextAttribute, IntAttribute
+from sublayers_server.model.registry.odm.doc import AbstractDocument
+from sublayers_server.model.registry.odm.fields import (
+    UniReferenceField, StringField, IntField, FloatField, ListField, EmbeddedDocumentField,
+)
 
 
 class ClassSkill(Root):
-    target = TextAttribute(default='', caption=u"Целевой навык", tags='client')
-    bonus_step = IntAttribute(default=0, caption=u"Шаг начисления бонуса", tags='client')
-    limit = IntAttribute(default=0, caption=u"Предел роста навыка", tags='client')
+    target = StringField(default='', caption=u"Целевой навык", tags='client')
+    bonus_step = IntField(default=0, caption=u"Шаг начисления бонуса", tags='client')
+    limit = IntField(default=0, caption=u"Предел роста навыка", tags='client')
 
 
 class Skill(Root):
-    value = IntAttribute(default=0, caption=u"Уровень навыка", tags='client')
-    limit = IntAttribute(default=100, caption=u"Предел прокачки навыка", tags='client')
-    mod = RegistryLink(default='reg://registry/rpg_settings/class_skill/empty_0', caption=u"Модификатор навыка")
-    description = TextAttribute(default=u'Игрвой навык персонажа',
-                                caption=u'Расширенное описание',
-                                tags='client')
+    value = IntField(default=0, caption=u"Уровень навыка", tags='client')
+    limit = IntField(default=100, caption=u"Предел прокачки навыка", tags='client')
+    mod = UniReferenceField(default='reg://registry/rpg_settings/class_skill/empty_0', caption=u"Модификатор навыка")
+    description = StringField(
+        default=u'Игрвой навык персонажа',
+        caption=u'Расширенное описание',
+        tags='client',
+    )
 
     def get_bonuses(self, v):
         if v >= self.mod.bonus_step:
@@ -46,10 +50,16 @@ class Skill(Root):
         return d
 
 
+class SkillPriceItem(AbstractDocument):
+    count = IntField(caption=u"Количество")
+    price = FloatField(caption=u"Цена")
+
+
 class BuySkill(Root):
-    value = IntAttribute(default=0, caption=u"Количество купленных очков навыка", tags='client')
-    limit = IntAttribute(default=3, caption=u"Предел покупки очков навыка", tags='client')
-    description = TextAttribute(default=u'Дополнительное очко навыка',
-                                caption=u'Расширенное описание',
-                                tags='client')
-    price = Attribute(caption=u'Таблица цен на очки навыков', tags='client')
+    value = IntField(default=0, caption=u"Количество купленных очков навыка", tags='client')
+    limit = IntField(default=3, caption=u"Предел покупки очков навыка", tags='client')
+    description = StringField(default=u"Дополнительное очко навыка", caption=u"Расширенное описание", tags='client')
+    price = ListField(
+        caption=u'Таблица цен на очки навыков', tags='client',
+        base_field=EmbeddedDocumentField(SkillPriceItem),
+    )
