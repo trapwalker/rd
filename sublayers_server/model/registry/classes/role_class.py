c1bb2a2ccaf49938238b3fbb010ed84843d265a5
svp <unknown>
2016-06-28 11:03:27 +0300

    reg/classes/role_class: Миграция на ODM

--- sublayers_server/model/registry/classes/role_class.py
+++ sublayers_server/model/registry/classes/role_class.py
@@ -5,22 +5,25 @@ log = logging.getLogger(__name__)
 
 
 from sublayers_server.model.registry.storage import Root
-from sublayers_server.model.registry.attr import Attribute, FloatAttribute, TextAttribute, IntAttribute
-from sublayers_server.model.registry.attr.link import RegistryLink
-from sublayers_server.model.registry.attr.inv import InventoryPerksAttribute
+from sublayers_server.model.registry.odm.fields import StringField, IntField, UniReferenceField, ListField
 
 
 class RoleClass(Root):
-    title = TextAttribute(caption=u'Название класса', tags='client')
-    icon = Attribute(caption=u'Пиктограмма класса', tags='client')
-    emblem = Attribute(caption=u'Эмблема класса', tags='client')
-    description = TextAttribute(caption=u'Расширенное описание класса', tags='client')
-    console_description = TextAttribute(caption=u'Расширенное описание класса', tags='client')
+    title = StringField(caption=u'Название класса', tags='client')
+    icon = StringField(caption=u'Пиктограмма класса', tags='client')  # todo: use specific field type
+    emblem = StringField(caption=u'Эмблема класса', tags='client')
+    description = StringField(caption=u'Расширенное описание класса', tags='client')
+    console_description = StringField(caption=u'Расширенное описание класса', tags='client')
 
-    class_skills = Attribute(caption=u"Список классовых навыков", tags='client')
-    money = IntAttribute(caption=u'Стартовое количество денег', tags='client')
-    start_car = RegistryLink(default=None, caption=u'Стартовая машинка для данного класса', tags="client")
-    start_perks = InventoryPerksAttribute(caption=u'Список доступных стартовых перков')
-
-    start_free_point_perks = IntAttribute(default=0, caption=u'Стартовые очки перков', tags='client')
-    start_free_point_skills = IntAttribute(default=0, caption=u'Стартовые очки навыков', tags='client')
+    class_skills = ListField(
+        caption=u"Список классовых навыков",
+        base_field=UniReferenceField('sublayers_server.model.registry.classes.skills.ClassSkill'),
+    )
+    money = IntField(caption=u'Стартовое количество денег', tags='client')
+    start_car = UniReferenceField(default=None, caption=u'Стартовая машинка для данного класса', tags="client")
+    start_perks = ListField(
+        caption=u"Список доступных стартовых перков",
+        base_field=UniReferenceField('sublayers_server.model.registry.classes.perks.Perk'),
+    )
+    start_free_point_perks = IntField(default=0, caption=u'Стартовые очки перков', tags='client')
+    start_free_point_skills = IntField(default=0, caption=u'Стартовые очки навыков', tags='client')
