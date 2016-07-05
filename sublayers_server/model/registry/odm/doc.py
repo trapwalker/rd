bd1f5d5c38977d5026fa5cf7eab6c8c7e81b5c65
svp <unknown>
2016-07-05 14:55:59 +0300

    reg/odm: from_son object links loading FIX (+todo)

--- sublayers_server/model/registry/odm/doc.py
+++ sublayers_server/model/registry/odm/doc.py
@@ -222,6 +222,8 @@ class AbstractDocument(Document):
 
     @classmethod
     def from_son(cls, dic, _is_partly_loaded=False, _reference_loaded_fields=None):
+        if isinstance(dic, basestring):
+            return dic  # todo: may be add async loading task to eventloop?
         klass_name = dic.get('__cls__')
         klass = cls.get_class(klass_name) if klass_name else cls  # tpdp: add warning if class not found
         # todo: Падать при инициализации, игнорировать с предупреждениями в процессе
