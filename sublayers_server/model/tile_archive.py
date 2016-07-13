5aa774380d0e82ae9229b67ca41da739ad9ec9c3
menkent <unknown>
2016-07-13 09:54:21 +0300

    server: теперь отдаётся архив 25 тайлами на 1 масштаб

--- sublayers_server/model/tile_archive.py
+++ sublayers_server/model/tile_archive.py
@@ -33,10 +33,10 @@ def get_tiles_admin(ax, ay):
         px, py, pz = start_tile.parent_by_lvl(level=zoom).xyz()
         os.mkdir(str(zoom))
         os.chdir(str(zoom))
-        for x in range(px-1, px+2):
+        for x in range(px-2, px+3):
             # создать папки X
             os.mkdir(str(x))
-            for y in range(py-1, py+2):
+            for y in range(py-2, py+3):
                 img_source = http.fetch(request=download_str(zoom, x, y), method="GET").body
                 file_like = cStringIO.StringIO(img_source)
                 with open(os.path.join(os.path.join(os.path.join(temp_dir, str(zoom)), str(x)), '{}.jpg'.format(y)), 'wb') as fdest:
