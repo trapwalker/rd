ae1a95fc8eee7c202fd6522f995b7e4f97c43238
svp <unknown>
2016-09-19 16:47:24 +0300

    reg/odm: __not_a_fields__ list as Registry class atribute ADD. reg/tree: Registry class registration method REFACTORING

--- sublayers_server/model/registry/odm/meta.py
+++ sublayers_server/model/registry/odm/meta.py
@@ -8,6 +8,9 @@ from motorengine.metaclasses import DocumentMetaClass, classproperty
 from sublayers_server.model.registry.odm.qs import CachebleQuerySet
 
 
+# todo: Решить вопрос с регистрацией в метаклассе всех (возможно одноименных) поддокументов
+
+
 class NodeMeta(DocumentMetaClass):
     _classes = {}
     _objects_cache = {}
@@ -19,19 +22,46 @@ class NodeMeta(DocumentMetaClass):
         """
         return cls._objects_cache
 
+    def get_class(cls, name):
+        subnames = name.split('.')
+        firstname = subnames[0]
+        subnames = subnames[1:]
+        c = cls._classes.get(firstname)
+        for subname in subnames:
+            if c is None:
+                break
+            c = getattr(c, subname, None)
+
+        if c is None:
+            raise NameError('Registry class {!r} is not found.'.format(name))
+
+        return c
+
     @property
     def key_fields(cls):
         return {field_name for field_name, field in cls._fields.items() if getattr(field, 'identify', None)}
 
-    def __new__(cls, name, bases, attrs):
-        new_class = super(NodeMeta, cls).__new__(cls, name, bases, attrs)
+    @classmethod
+    def _registry(cls, name, new_class):
+        assert name not in cls._classes, 'Registry class {} already registered.'.format(name)
         cls._classes[name] = new_class
-        setattr(new_class, 'objects', classproperty(lambda *args, **kw: CachebleQuerySet(new_class)))
 
+    def __new__(cls, name, bases, attrs):
+        not_a_fields = frozenset(attrs.pop('__not_a_fields__', []))
+        for base in bases:
+            if hasattr(base, '__not_a_fields__'):
+                not_a_fields |= frozenset(base.__not_a_fields__)
+
+        new_class = super(NodeMeta, cls).__new__(cls, name, bases, attrs)
+        setattr(new_class, 'objects', classproperty(lambda *args, **kw: CachebleQuerySet(new_class)))
         setattr(new_class, '__lazy__', False)
         setattr(new_class, '__collection__', 'registry')
-
+        setattr(new_class, '__not_a_fields__', not_a_fields)
+        cls._registry(name, new_class)
         return new_class
 
-    def get_class(cls, name):
-        return cls._classes.get(name)
+
+class SubclassMeta(NodeMeta):
+    @classmethod
+    def _registry(cls, name, new_class):
+        pass
