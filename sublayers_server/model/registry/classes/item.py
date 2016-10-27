937601c3453cebf36b00190e9edfd150391bdceb
svp <unknown>
2016-10-27 18:04:17 +0300

    reg/items: Проверка на переполнение стека итема при инстанцировании. Обрезание.

--- sublayers_server/model/registry/classes/item.py
+++ sublayers_server/model/registry/classes/item.py
@@ -50,6 +50,12 @@ class Item(Root):
         assert not self.uri, 'Item has URI {!r} and cannot splited'.format(self.uri)
         return self.instantiate(amount=count)
 
+    def instantiate(self, **kw):
+        inst = super(Item, self).instantiate(**kw)
+        if self.amount > self.stack_size:
+            log.warning('Item stack owerflow truncated: {self.amount}>{self.stack_size} in {self}'.format(self=self))
+            self.amount = self.stack_size
+
     # def __str__(self):
     #     return '{}<{}/{}>'.format(self.__class__.__name__, self.activate_type, self.amount)
 
