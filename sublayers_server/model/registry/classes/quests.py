e933e9bd55295e563126641429123fe5d51ed6e7
svp <unknown>
2016-11-07 15:39:07 +0300

    quests: Устранение отладочного кода, связанного с ошибкой "списка списков"

--- sublayers_server/model/registry/classes/quests.py
+++ sublayers_server/model/registry/classes/quests.py
@@ -676,11 +676,7 @@ class DeliveryQuest(Quest):
 
     def as_client_dict(self):
         d = super(DeliveryQuest, self).as_client_dict()
-        items = []
-        for delivery_rec in self.delivery_set:
-            items.append(delivery_rec.as_client_dict())
-
         d.update(
-            delivery_set=items,  # [delivery_rec.as_client_dict() for delivery_rec in self.delivery_set],
+            delivery_set=[delivery_rec.as_client_dict() for delivery_rec in self.delivery_set],
         )
         return d
