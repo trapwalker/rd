64cefe0fdc41ec38dc6f1cdaf5c7a598f4969ca2
menkent <unknown>
2016-11-17 15:10:45 +0300

    server: блокировка окон пати и журнала в режиме быстрой игры. прототипы заглушек

--- sublayers_server/handlers/main_menu_journal.py
+++ sublayers_server/handlers/main_menu_journal.py
@@ -4,8 +4,14 @@ import logging
 log = logging.getLogger(__name__)
 
 from sublayers_common.handlers.base import BaseHandler
+from tornado.options import options
 
 
 class MenuJournalHandler(BaseHandler):
     def get(self):
-        self.render("menu/journal_window.html")
+        if options.mode == 'basic':
+            self.render("menu/journal_window.html")
+        elif options.mode == 'quick':
+            self.render("menu/quick_mode_plug.html")
+        else:
+            self.send_error(status_code=404)
