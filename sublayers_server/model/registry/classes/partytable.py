2a51adf433351155ba5727999b063343e7d69f18
svp <unknown>
2017-06-21 12:30:14 +0300

    reg/migration: partytable migration FIX

--- sublayers_server/model/registry/classes/partytable.py
+++ sublayers_server/model/registry/classes/partytable.py
@@ -3,19 +3,19 @@
 import logging
 log = logging.getLogger(__name__)
 
-from sublayers_server.model.registry.classes.exptable import Pair
-from sublayers_server.model.registry.tree import Root
-from sublayers_server.model.registry.odm.fields import UniReferenceField, IntField, FloatField, ListField, EmbeddedDocumentField
+from sublayers_server.model.registry_me.tree import Node
+
+from mongoengine import ListField, EmbeddedDocumentField
 
 
-class PartyTables(Root):
+class PartyTables(Node):
     leading = ListField(
         caption=u'Таблица расстояний между локациями',
-        base_field=EmbeddedDocumentField(embedded_document_type=Pair),
+        field=EmbeddedDocumentField(document_type='sublayers_server.model.registry_me.classes.exptable.Pair'),
     )
 
     def get_party_capacity(self, agent):
-        agent_leading = agent.leading.calc_value()
+        agent_leading = agent.profile.leading.calc_value()
         result = 0
         for pair in self.leading:
             if pair.k <= agent_leading:
