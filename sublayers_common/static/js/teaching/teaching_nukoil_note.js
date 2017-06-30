c954dc198ca07795a7587d94830be57a33c91d1a
menkent <unknown>
2017-06-30 11:58:03 +0300

    client: f5 при обучении у нукойла

--- sublayers_common/static/js/teaching/teaching_nukoil_note.js
+++ sublayers_common/static/js/teaching/teaching_nukoil_note.js
@@ -22,13 +22,17 @@ var NukoilTeachingNote = (function (_super) {
 
     NukoilTeachingNote.prototype.on_enter_location = function() {
         _super.prototype.on_enter_location.call(this);
-        this.needed_building = locationManager.buildings.nucoil;
+        this.needed_building = locationManager.buildings.nukeoil;
         this.needed_npc = locationManager.npc['reg--registry-institutions-gas_station'];
     };
 
     NukoilTeachingNote.prototype.redraw = function() {
         if (!locationManager.in_location_flag) return;
         var active_place = locationManager.get_current_active_place();
+
+        if(!this.needed_building) this.needed_building = locationManager.buildings.nukeoil;
+        if(!this.needed_npc) this.needed_npc = locationManager.npc['reg--registry-institutions-gas_station'];
+
         if ((this.needed_screen_name != locationManager.active_screen_name) ||
             ((active_place != this.needed_building) &&
              (active_place != this.needed_npc) &&
