54f244842676c5c3663270d2e4c1390b4bd6462b
menkent <unknown>
2017-07-19 12:20:49 +0300

    server: квесты доставки

--- sublayers_server/model/registry_me/classes/quests2.py
+++ sublayers_server/model/registry_me/classes/quests2.py
@@ -2,18 +2,3 @@
 
 import logging
 log = logging.getLogger(__name__)
-
-from sublayers_server.model.poi_loot_objects import CreatePOILootEvent, CreatePOICorpseEvent, QuestPrivatePOILoot
-from sublayers_server.model.inventory import ItemState
-from sublayers_server.model.vectors import Point
-from sublayers_server.model.registry_me.classes.quests import Quest, QuestRange, MarkerMapObject
-from sublayers_server.model.registry_me.classes.quests1 import DeliveryQuestSimple
-from sublayers_server.model.registry_me.tree import (
-    Subdoc, ListField, IntField, FloatField, EmbeddedDocumentField, EmbeddedNodeField, PositionField, UUIDField,
-)
-from sublayers_server.model.registry_me.classes import notes
-
-import random
-
-
-
