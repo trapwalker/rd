4aca80f0a5199e352739d8c8adeba3d04a7cd4e8
svp <unknown>
2017-07-19 17:40:11 +0300

    tools/cli: profiles reset option ADD

--- cli/_common.py
+++ cli/_common.py
@@ -5,6 +5,7 @@ log = logging.getLogger(__name__)
 
 
 from sublayers_server.model.registry_me.classes.agents import Agent
+from sublayers_common.user_profile import User
 
 from sublayers_common.ctx_timer import Timer
 
@@ -13,6 +14,12 @@ def agents_clean():
     with Timer() as tm:
         deleted_agents_count = Agent.objects.all().delete()
         log.info('All stored agents deleted: %s (%.3fs)', deleted_agents_count, tm.duration)
+
+
+def profiles_reset():
+    with Timer() as tm:
+        res = User.objects.update(registration_status="nickname")
+        log.info('All profiles reset to 2-nd registration step: %s (%.3fs)', res, tm.duration)
 
 
 def save_to_file(registry, dest):
