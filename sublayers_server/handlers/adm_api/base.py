c26d621ef965f6bb45c74944979c48adc656c089
svp <unknown>
2017-07-25 17:26:45 +0300

    server/api: ADM API localhost only access granted FIX

--- sublayers_server/handlers/adm_api/base.py
+++ sublayers_server/handlers/adm_api/base.py
@@ -16,7 +16,11 @@ class AdmAPIHandler(AuthHandlerMixin):
 
     def check_xsrf_cookie(self):
         """Disable checking XSRF for Adm API handlers"""
-        pass
+        host = self.request.host
+        remote_ip = self.request.remote_ip
+        if host != remote_ip and remote_ip not in ['localhost', '127.0.0.1', '::1']:
+            log.warning('Call %s by %s from %s', self.classname, self.request.host, self.request.remote_ip)
+            super(AdmAPIHandler, self).check_xsrf_cookie()
 
     def initialize(self, *av, **kw):
         super(AdmAPIHandler, self).initialize(*av, **kw)
