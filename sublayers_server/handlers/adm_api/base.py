043b80f9fd6ba02cc94e47762e31f0338bebdc09
svp <unknown>
2017-07-25 19:16:33 +0300

    tools/cli: nonlocal adm API call FORBIDDEN

--- sublayers_server/handlers/adm_api/base.py
+++ sublayers_server/handlers/adm_api/base.py
@@ -8,17 +8,22 @@ log = logging.getLogger(__name__)
 from sublayers_common.handlers.base import AuthHandlerMixin
 from sublayers_server import engine_server
 
-from tornado.web import RequestHandler
+from tornado.web import RequestHandler, HTTPError
+
+
+LOCALHOST = frozenset(['localhost', '127.0.0.1', '::1'])
 
 
 class AdmAPIHandler(AuthHandlerMixin):
     _name_ = None
 
+    def is_local_request(self):
+        remote_ip = self.request.remote_ip
+        return remote_ip in LOCALHOST  # or remote_ip == self.request.host
+
     def check_xsrf_cookie(self):
         """Disable checking XSRF for Adm API handlers"""
-        host = self.request.host
-        remote_ip = self.request.remote_ip
-        if host != remote_ip and remote_ip not in ['localhost', '127.0.0.1', '::1']:
+        if not self.is_local_request():
             log.warning('Call %s by %s from %s', self.classname, self.request.host, self.request.remote_ip)
             super(AdmAPIHandler, self).check_xsrf_cookie()
 
@@ -30,4 +35,11 @@ class AdmAPIHandler(AuthHandlerMixin):
         return self._name_ or self.classname
 
     def prepare(self):
+        if not self.is_local_request():
+            log.warning(
+                'Nonlocal Adm API call "%s" by "%s" from "%s" DENIED!',
+                self.classname, self.request.host, self.request.remote_ip,
+            )
+            raise HTTPError(403)
+
         log.info('ADMIN:: %s', self.command_name)
