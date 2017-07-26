05912439786798bd7ce44a0e7455c221781c745b
ANTiPodec <unknown>
2017-07-26 17:33:53 +0300

    client: добавлены bmw_320i_e21, delorean_dmc_12, gmc_pd_4501_scenicruiser

--- sublayers_server/handlers/site_api.py
+++ sublayers_server/handlers/site_api.py
@@ -39,7 +39,7 @@ class APIGetCarInfoHandler2(BaseHandler):
     def get(self):
         log.error('Error! Site API called!')
         #uri = self.get_argument('uri', None)
-        uri = 'reg:///registry/mobiles/cars/middle/sports/delorean_dmc12'  # todo: ##fix
+        uri = 'reg:///registry/mobiles/cars/middle/sports/04_delorean_dmc_12'  # todo: ##fix
         if not uri:
             self.send_error(404)
             return
