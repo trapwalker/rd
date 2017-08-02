9e1af5af6a8aef1af96f304f64873ac12c396702
svp <unknown>
2017-08-02 21:36:03 +0300

    reg: agents testing script

---
+++ sublayers_server/model/registry_me/agent_test.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function
+import sys
+import logging
+log = logging.getLogger(__name__)
+
+if __name__ == '__main__':
+    sys.path.append('../../..')
+    log = logging.getLogger()
+    try:
+        import coloredlogs
+        coloredlogs.DEFAULT_FIELD_STYLES['levelname']['color'] = 'green'
+        coloredlogs.install(level=logging.DEBUG, fmt='%(levelname)-8s| %(message)s')
+    except ImportError:
+        log.level = logging.DEBUG
+        _hndl = logging.StreamHandler(sys.stderr)
+        _hndl.setFormatter(logging.Formatter('%(levelname)-8s| %(message)s'))
+        log.addHandler(_hndl)
+
+from sublayers_server.model.registry_me.classes.agents import Agent
+from sublayers_server.model.registry_me import classes
+from sublayers_common.ctx_timer import Timer, T
+from sublayers_server.model.registry_me.tree import (
+    connect,
+    Node, get_global_registry, ListField, EmbeddedNodeField, Registry, RegistryLinkField, StringField,
+    GenericEmbeddedDocumentField,
+    DynamicSubdoc,
+    STAT,
+    field_getter_decorator,
+)
+
+from pprint import pprint as pp
+import os
+
+
+def test1(reload, save_loaded):
+    reg = get_global_registry(path=u'../../../sublayers_world', reload=reload, save_loaded=save_loaded)
+    agents = Agent.objects.limit(30)
+    repeat = True
+    a, i = None, 0
+    while repeat:
+        a = None
+        print('{:5}: '.format(i), end='')
+        try:
+            a = agents.next()
+            print(a.login)
+
+        except StopIteration:
+            repeat = False
+        except Exception as e:
+            print('ERROR({e.__class__.__name__}): {e}'.format(e=e))
+        finally:
+            i += 1
+
+    globals().update(locals())
+
+
+if __name__ == '__main__':
+    import math
+    db_name = 'rd' #+ 't'
+    db = connect(db=db_name)
+    log.info('Use {db_name!r} db'.format(**locals()))
+
+    rel = 0
+
+    with T('=== Test DONE ==='):
+        test1(reload=rel, save_loaded=True)
