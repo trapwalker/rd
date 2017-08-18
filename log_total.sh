d2072db52fb1b8b9b0305646c8d460fcefd7c681
svp <unknown>
2017-08-18 13:35:54 +0300

    tools: log_total script include quick server logs

--- log_total.sh
+++ log_total.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-tail -F sublayers_server/log/server.log sublayers_site/log/server.log
+tail -F sublayers_server/log/server.log sublayers_server/log_quick/server.log sublayers_site/log/server.log
