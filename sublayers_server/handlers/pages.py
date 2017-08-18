2fa1a7a74b7ffc55ace0e408d17ad30616e7832e
svp <unknown>
2017-08-18 13:22:01 +0300

    server/engine: Таймацт при слишком частом обновлении страницы

--- sublayers_server/handlers/pages.py
+++ sublayers_server/handlers/pages.py
@@ -29,34 +29,62 @@ def frequency_handler_calling_limiter_deco(key=lambda *av, **kw: None):
 
 class PlayHandler(BaseHandler):
     _frequency_stat = {}
-    def _is_frequency_fail(self):
+
+    def _frequency_delay(self, user_id):
+        delay_to_forget = 90
+        max_call_count_to_sleep = 7 #10
+
+        frequency_stat = self._frequency_stat
+        stat = frequency_stat.setdefault((self.__class__.__name__, 'get', user_id), Counter())
+        t = get_time()
+        last = stat['last']
+        delta = t - last
+        stat['last'] = t
+        if delta > delay_to_forget:
+            log.debug('== Reset handler call status by user %r', user_id)
+            stat['count'] = 1
+            stat['sum'] = 0
+        else:
+            stat['count'] += 1
+            stat['sum'] += delta
+            _sum = stat['sum']
+            cnt = stat['count']
+            if cnt > max_call_count_to_sleep:
+                time_to_sleep = 20
+                log.warning(
+                    'User by id %r is too frequency call handler: %s times in last %ss. Need to sleep %ss.',
+                    user_id, cnt, _sum, time_to_sleep,
+                )
+                return time_to_sleep
+            log.debug('== Frequently calling status by user %r: %r', user_id, stat)
+
+    def initialize(self):
+        super(PlayHandler, self).initialize()
+        self._break_handler = False
+
+    def on_connection_close(self):
+        super(PlayHandler, self).on_connection_close()
+        log.debug('== User %r is closed connection.', self.get_secure_cookie("user"))
+        self._break_handler = True
+
+    @tornado.gen.coroutine
+    def get(self):
         user_id = self.get_secure_cookie("user")
         if not user_id:
             self.redirect(self.get_login_url())
-            return True
-
-    def get(self):
-        if self._is_frequency_fail():
             return
 
-        _frequency_stat = self._frequency_stat
-        _stat = _frequency_stat.setdefault((self.__class__.__name__, 'get', user_id), Counter())
-        _t = get_time()
-        _last = _stat['last']
-        _delta = _t - _last
-        _stat['last'] = _t
-        if _delta > 90:
-            _stat['count'] = 1
-            _stat['sum'] = 0
-        else:
-            _stat['count'] += 1
-            _stat['sum'] += _delta
-
-
-
-        _last_time = _stat['last']
-
-
+        _time_to_sleep = self._frequency_delay(user_id)
+        if _time_to_sleep:
+            yield tornado.gen.sleep(_time_to_sleep)
+            log.debug(
+                'Ok. User %r is waked up after %ss %s',
+                user_id,
+                _time_to_sleep,
+                'but connection already closed' if self._break_handler else '',
+            )
+            if self._break_handler:
+                raise tornado.web.HTTPError(417)
 
         user = self.current_user
         # todo: ##REFACTORING
@@ -71,10 +99,10 @@ class PlayHandler(BaseHandler):
                 self.redirect(self.get_login_url())
                 return
             time = self.application.srv.get_time()
-            connection_delay = agent.get_connection_delay(time=time)
-            if connection_delay is None:
-                self.redirect('/static/connection_trouble.html')
-                return
+            connection_delay = 0 #agent.get_connection_delay(time=time)
+            # if connection_delay is None:
+            #     self.redirect('/static/connection_trouble.html')
+            #     return
 
             coord = agent.get_loading_coord(time=time)
 
