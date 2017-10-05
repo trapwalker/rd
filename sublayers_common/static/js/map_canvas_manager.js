944fb1a946480cd8abb6b690498ddbe57a2f30e2
abbir <unknown>
2017-10-05 16:15:08 +0300

    client: доработки динамической камеры.

--- sublayers_common/static/js/map_canvas_manager.js
+++ sublayers_common/static/js/map_canvas_manager.js
@@ -143,6 +143,11 @@ var MapCanvasManager = (function(_super){
     };
 
     MapCanvasManager.prototype.redraw = function(time) {
+        function easeInQuart(x, t, b, c, d) { // x: percent of animate, t: current time, b: begInnIng value, c: change In value, d: duration
+            //return c * (t /= d) * t * t * t + b;
+            return -c * Math.cos(t/d * (Math.PI/2)) + c + b;
+        }
+
         if(! this.is_canvas_render) return;
 
         var map_size = mapManager.getMapSize();
@@ -150,8 +155,8 @@ var MapCanvasManager = (function(_super){
             this.cur_map_size = map_size;
             this._central_point = mulScalVector(mapManager.getMapSize(), 0.5);
             var s = Math.min(map_size.x, map_size.y) * 0.5;
-            this._lock_radius1 = s * 0.5;
-            this._lock_radius2 = s;
+            this._lock_radius1 = s * 1.3;
+            this._lock_radius2 = s * 1.8;
         }
 
         // Расчет сдвига карты от указателя мыши
@@ -159,17 +164,30 @@ var MapCanvasManager = (function(_super){
         this.target_mouse_shift = subVector(cursor_point, this._central_point);
         var sqr_abs = this.target_mouse_shift.sqr_abs();
 
+        // Бублик
+        var d_len = this._lock_radius2 - this._lock_radius1;
         if (sqr_abs < this._lock_radius1 * this._lock_radius1)
             this.target_mouse_shift = new Point(0, 0);
         else if (sqr_abs > this._lock_radius2 * this._lock_radius2)
-            this.target_mouse_shift = normVector(this.target_mouse_shift, this._lock_radius1);  // потому что this._lock_radius1 = this._lock_radius2 / 2
+            this.target_mouse_shift = normVector(this.target_mouse_shift, d_len);  // потому что this._lock_radius1 = this._lock_radius2 / 2
         else
             this.target_mouse_shift = normVector(this.target_mouse_shift, (Math.sqrt(sqr_abs) - this._lock_radius1));
 
+        // Не бублик
+        //if (sqr_abs > this._lock_radius2 * this._lock_radius2)
+        //    this.target_mouse_shift = normVector(this.target_mouse_shift, this._lock_radius2);
+        //var d_len = this._lock_radius2;
+
+        // Градиент
+        var len = this.target_mouse_shift.abs();
+        var new_len = easeInQuart(0, len, 0, d_len, d_len);
+        this.target_mouse_shift = normVector(this.target_mouse_shift, new_len);
+
+        // Медленное движение
         var d_shift = subVector(this.target_mouse_shift, this.current_mouse_shift);
         if (settingsManager.options.dynamic_camera.currentValue)
             if (d_shift.sqr_abs() > 10) {
-                d_shift.x = d_shift.x * 0.2; d_shift.y = d_shift.y * 0.2;
+                d_shift.x = d_shift.x * 0.1; d_shift.y = d_shift.y * 0.1;
                 this.current_mouse_shift = summVector(this.current_mouse_shift, d_shift);
             }
             else
