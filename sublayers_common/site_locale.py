ac32fc7ecbc8ec4656df8e8d5667080e1edde00e
svp <unknown>
2017-10-26 21:05:59 +0300

    localization: localization map building time logging

--- sublayers_common/site_locale.py
+++ sublayers_common/site_locale.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
+from __future__ import absolute_import
 
 import logging
 log = logging.getLogger(__name__)
 
+import sys
 from os import walk
 from os.path import join
 import yaml
@@ -26,26 +28,29 @@ def load_locale_objects(path, *other_paths):
     paths.extend(other_paths)
     walks = chain(*map(walk, paths))
 
-    for r, d, files in walks:
-        for f in files:
-            if fnmatch(f, '*.lang.yaml'):
-                fn = join(r, f)
-                with open(fn) as data_file:
-                    data = yaml.load(data_file)
-                    locale = data.pop('locale', None)
-                    _prefix = data.pop('__prefix', None)
-                    if locale:
-                        locale_lang_dict = locale_objects.setdefault(locale, {'locale': locale})
-                        # Поиск конфликтов по ключам
-                        old_keys = frozenset(locale_lang_dict.keys())
-                        new_keys = frozenset(data.keys())
-                        conflict_keys = old_keys & new_keys
-                        if conflict_keys:
-                            log.warning('Locale keys conflict in %r: %r', fn, list(conflict_keys))
+    with Timer() as tm:
+        for r, d, files in walks:
+            for f in files:
+                if fnmatch(f, '*.lang.yaml'):
+                    fn = join(r, f)
+                    with open(fn) as data_file:
+                        data = yaml.load(data_file)
+                        locale = data.pop('locale', None)
+                        _prefix = data.pop('__prefix', None)
+                        if locale:
+                            locale_lang_dict = locale_objects.setdefault(locale, {'locale': locale})
+                            # Поиск конфликтов по ключам
+                            old_keys = frozenset(locale_lang_dict.keys())
+                            new_keys = frozenset(data.keys())
+                            conflict_keys = old_keys & new_keys
+                            if conflict_keys:
+                                log.warning('Locale keys conflict in %r: %r', fn, list(conflict_keys))
 
-                        locale_lang_dict.update(data)
-                    else:
-                        log.warning('Not define locale in file: %r', fn)
+                            locale_lang_dict.update(data)
+                        else:
+                            log.warning('Not define locale in file: %r', fn)
+
+    log.debug('Localization files collect DONE ({:.2f}s)'.format(tm.duration))
 
     for locale, locale_object in locale_objects.items():
         # Сборка всех встреченных ключей с указанием перечня поддерживаемых локалей:
