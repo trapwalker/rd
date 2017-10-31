9dd62aad3abb515ee0a270ae56e85994ba195c79
menkent <unknown>
2017-10-31 12:13:01 +0300

    server: ClassQuestSetMechanicItems

--- sublayers_server/model/registry_me/classes/mobiles.py
+++ sublayers_server/model/registry_me/classes/mobiles.py
@@ -212,6 +212,9 @@ class Mobile(Node):
     def iter_weapons(self):
         return (v for attr, v in self.iter_slots(tags={'armorer'}) if isinstance(v, Weapon))
 
+    def iter_mechanic_items(self):
+        return (v for attr, v in self.iter_slots(tags={'mechanic'}) if isinstance(v, MechanicItem))
+
     def iter_armorer_slots_name(self):
         return (attr for attr, v in self.iter_slots(tags={'armorer'}))
 
