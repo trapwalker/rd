81c8435295a6966e3cc5219d39b60012222dace2
svp <unknown>
2017-11-03 02:55:33 +0300

    cli: rd backup --restart option; 'dest' param FIX

--- cli/backup.py
+++ cli/backup.py
@@ -7,7 +7,7 @@ log = logging.getLogger(__name__)
 
 
 from cli.root import root
-from cli.control import save
+from cli.control import save, restart as do_restart
 from sublayers_common.service_tools import run
 
 import click
@@ -21,18 +21,20 @@ import shutil
 @click.option('--dest', '-d', 'dest', default=None, type=click.Path(file_okay=False, writable=True))
 @click.option('--save' ,'-s', 'save_server', is_flag=True, default=False, help='Backup server database')
 @click.option('--host' ,'-h', 'host', default='http://localhost:8000', type=click.STRING, help='Host to send the command save')
+@click.option('--restart', is_flag=True, default=False, help='Restart servers after backup')
 @click.pass_context
-def backup_command(ctx, dest, save_server, host):
+def backup_command(ctx, dest, save_server, host, restart):
     """Start service"""
 
     if ctx.invoked_subcommand:
         return
 
     db = ctx.obj['db']
-    backup(db, dest, save_server, host)
+    backup(db, dest, save_server, host, restart)
 
 
-def backup(db, dest, save_server, host):
+def backup(db, dest, save_server, host, restart):
+    dest = dest or '.'
     if save_server:
         save(host=host)  # todo: configure server host
 
@@ -41,13 +43,17 @@ def backup(db, dest, save_server, host):
     zf = '{fn}.zip'.format(**locals())
     log.info('Backup db of {host}'.format(**locals()))
     try:
+        # TODO: Use temp folder
         log.debug(run('mongodump -d {db} -o {fn}'.format(**locals()).split()))
     except subprocess.CalledProcessError as e:
         log.error(e)
     else:
-        with zipfile.ZipFile(zf, 'w') as z:
+        with zipfile.ZipFile(os.path.join(dest, zf), 'w') as z:
             for root, dirs, files in os.walk(fn):
                 for file in files:
                     z.write(os.path.join(root, file))
+
+        if restart:
+            do_restart(host=host)
     finally:
         shutil.rmtree(fn, ignore_errors=True)
