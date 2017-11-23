e9434afbbf3c69508f21980818414a160af96fd5
svp <unknown>
2017-11-23 19:01:28 +0300

    deploy: mongo js utils add - user lookup function

--- deploy/.mongorc.js
+++ deploy/.mongorc.js
@@ -7,3 +7,36 @@ function rmu(login) {
     print('From user:')
     printjson(db.user.remove({name: login}));
 };
+
+
+function lookup(anyid) {
+    db = db.getSiblingDB('rd');
+    print('Lookup by ' + anyid);
+
+    var u = db.user.find({$or: [
+            {"auth.steam.social_id": anyid},
+            {"auth.standard.email": anyid},
+            {"name": anyid}
+        ]});
+
+    var res = {
+        users: u.toArray()
+    };
+
+    //print("User found: " + u.count());
+    //printjson(res.users);
+
+    res.users.forEach(
+        function(user, i, arr) {
+            print("### USER[" + i + "]: ##########################################");
+            printjson(user);
+            user.agents = db.agent.find({login: user.name}).toArray();
+            user.agents.forEach(function(a, j, ags){
+           print("=== AGENT[" + j + "] ==================================================");
+           printjson(a);
+            });
+        }
+    );
+
+    return res;
+};
