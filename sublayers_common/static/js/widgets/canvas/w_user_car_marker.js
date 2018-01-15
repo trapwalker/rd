0f754174d4bdc3222662a2b55fa39728de414302
Sedhumani <unknown>
2018-01-15 16:03:33 +0300

    static: статика poi

--- sublayers_common/static/js/widgets/canvas/w_user_car_marker.js
+++ sublayers_common/static/js/widgets/canvas/w_user_car_marker.js
@@ -1,4 +1,4 @@
-var ConstMaxLengthToMoveMarker = 6;
+﻿var ConstMaxLengthToMoveMarker = 6;
 var ConstMaxAngleToMoveMarker = Math.PI / 90.;
 
 
@@ -518,6 +518,9 @@ var WCanvasStaticTownMarker = (function (_super) {
                         break;
                     case 'reg:///registry/poi/locations/towns/painted_rock':
                         icon_name = 'city_painted_rock';
+                        break;
+                    case 'reg:///registry/poi/locations/towns/los_perros':
+                        icon_name = 'los_jetas';
                         break;
                     case 'reg:///registry/poi/locations/towns/nukeoil_006':
                     case 'reg:///registry/poi/locations/towns/nukeoil_005':
